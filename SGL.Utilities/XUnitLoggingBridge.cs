6b0c7798816c103054445757ee7f1775dbed29ee
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-20 13:56:58 +0200

    Implemented XUnitLoggingProvider to bridge between ILogger-based logging and xUnit's ITestOutputHelper.

---
+++ SGL.Utilities/XUnitLoggingBridge.cs
@@ -0,0 +1,60 @@
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace SGL.Analytics.Client.Tests {
+
+	public class XUnitLoggingProvider : ILoggerProvider {
+		private ITestOutputHelper output;
+
+		public class XUnitLogger : ILogger {
+			private ITestOutputHelper output;
+			private string categoryName;
+			private LoggerExternalScopeProvider scopes = new LoggerExternalScopeProvider();
+
+			public XUnitLogger(ITestOutputHelper output, string categoryName) {
+				this.output = output;
+				this.categoryName = categoryName;
+			}
+
+			public IDisposable BeginScope<TState>(TState state) {
+				return scopes.Push(state);
+			}
+
+			public bool IsEnabled(LogLevel logLevel) {
+				return logLevel < LogLevel.None;
+			}
+
+			public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) {
+				StringBuilder builder = new StringBuilder();
+				builder.AppendFormat("{0} [{1}] {2}", logLevel.ToString(), categoryName, formatter(state, exception));
+				if (exception != null) {
+					builder.Append(exception);
+				}
+				builder.Append(" ");
+				scopes.ForEachScope((scope, sb) => sb.AppendFormat("<{0}>", scopes), builder);
+				output.WriteLine(builder.ToString());
+			}
+		}
+
+		public XUnitLoggingProvider(ITestOutputHelper output) {
+			this.output = output;
+		}
+
+		public ILogger CreateLogger(string categoryName) {
+			return new XUnitLogger(output, categoryName);
+		}
+
+		public void Dispose() { }
+	}
+
+	public static class XUnitLoggingExtensions {
+		public static void AddXUnit(this ILoggingBuilder builder, ITestOutputHelper output) {
+			builder.AddProvider(new XUnitLoggingProvider(output));
+		}
+	}
+}
