c88db37cbd1d91c3f0c846005004f02ec9112f58
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-25 17:47:32 +0200

    Fixed bug where length parameter was not correctly used in StringGenerator.

--- SGL.Utilities/StringGenerator.cs
+++ SGL.Utilities/StringGenerator.cs
@@ -11,10 +11,10 @@ namespace SGL.Analytics.Utilities {
 		private static char[] charactersWithSpace = Enumerable.Range('A', 26).Concat(Enumerable.Range('a', 26)).Concat(Enumerable.Range('0', 10)).Append(' ').Select(c => (char)c).ToArray();
 
 		public static string GenerateRandomString(int length) {
-			return new string(Enumerable.Range(0, 256).Select(_ => charactersWithSpace[rnd.Next(charactersWithSpace.Length)]).ToArray());
+			return new string(Enumerable.Range(0, length).Select(_ => charactersWithSpace[rnd.Next(charactersWithSpace.Length)]).ToArray());
 		}
 		public static string GenerateRandomWord(int length) {
-			return new string(Enumerable.Range(0, 256).Select(_ => wordCharacters[rnd.Next(wordCharacters.Length)]).ToArray());
+			return new string(Enumerable.Range(0, length).Select(_ => wordCharacters[rnd.Next(wordCharacters.Length)]).ToArray());
 		}
 	}
 }
