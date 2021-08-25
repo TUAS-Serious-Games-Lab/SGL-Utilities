cd278b0f7bf4371a8d5c5f130f268b76a7619e02
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-08-25 17:45:39 +0200

    Added a method to generate single-word random strings, i.e. without spaces.

--- SGL.Utilities/StringGenerator.cs
+++ SGL.Utilities/StringGenerator.cs
@@ -7,10 +7,14 @@ using Xunit.Abstractions;
 namespace SGL.Analytics.Utilities {
 	public static class StringGenerator {
 		private static Random rnd = new Random();
-		private static char[] characters = Enumerable.Range('A', 26).Concat(Enumerable.Range('a', 26)).Concat(Enumerable.Range('0', 10)).Append(' ').Select(c => (char)c).ToArray();
+		private static char[] wordCharacters = Enumerable.Range('A', 26).Concat(Enumerable.Range('a', 26)).Concat(Enumerable.Range('0', 10)).Select(c => (char)c).ToArray();
+		private static char[] charactersWithSpace = Enumerable.Range('A', 26).Concat(Enumerable.Range('a', 26)).Concat(Enumerable.Range('0', 10)).Append(' ').Select(c => (char)c).ToArray();
 
 		public static string GenerateRandomString(int length) {
-			return new string(Enumerable.Range(0, 256).Select(_ => characters[rnd.Next(characters.Length)]).ToArray());
+			return new string(Enumerable.Range(0, 256).Select(_ => charactersWithSpace[rnd.Next(charactersWithSpace.Length)]).ToArray());
+		}
+		public static string GenerateRandomWord(int length) {
+			return new string(Enumerable.Range(0, 256).Select(_ => wordCharacters[rnd.Next(wordCharacters.Length)]).ToArray());
 		}
 	}
 }
