9a8249ec2d73786f5cb6be319f141d717ff621d4
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-10 19:56:57 +0200

    Implemented JwtAuthenticationExtensions.UseJwtBearerAuthentication.

---
+++ SGL.Utilities.Backend.Security/JwtAuthenticationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.Security {
+	public static class JwtAuthenticationExtensions {
+		public static IServiceCollection UseJwtBearerAuthentication(this IServiceCollection services, IConfiguration config) {
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+				options.TokenValidationParameters = new TokenValidationParameters {
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidateLifetime = true,
+					ValidateIssuerSigningKey = true,
+					ValidIssuer = config["Jwt:Issuer"],
+					ValidAudience = config["Jwt:Audience"],
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:SymmetricKey"]))
+				};
+				options.SaveToken = true;
+			});
+			return services;
+		}
+	}
+}
