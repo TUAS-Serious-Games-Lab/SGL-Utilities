427193f7cb28d84177e2a069dd7761f4a5f4d9a6
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-10 19:57:56 +0200

    Implemented SecretGenerator.

---
+++ SGL.Utilities/SecretGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Utilities {
+	public class SecretGenerator {
+		private RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+		private object lockObject = new object();
+		private SecretGenerator() { }
+
+		public static SecretGenerator Instance = new SecretGenerator();
+
+		public string GenerateSecret(int bytes) {
+			byte[] buff = new byte[bytes];
+			lock (lockObject) {
+				rngCsp.GetBytes(buff);
+			}
+			return Convert.ToBase64String(buff);
+		}
+	}
+}
