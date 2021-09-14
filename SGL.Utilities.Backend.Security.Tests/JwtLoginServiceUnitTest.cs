ebb30e48be0da62435a957a396429cd2ff53e3a1
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-14 14:51:04 +0200

    Added test case JwtLoginServiceIssuesValidTokenForValidCredentials as unit test for JwtLoginService.

---
+++ SGL.Utilities.Backend.Security.Tests/JwtLoginServiceUnitTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using SGL.Analytics.TestUtilities;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace SGL.Analytics.Backend.Security.Tests {
+	public class JwtLoginServiceUnitTest {
+		private ITestOutputHelper output;
+		private ILoggerFactory loggerFactory;
+		private JwtOptions options;
+		private JwtLoginService loginService;
+		private JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+		private class User { }
+
+		public JwtLoginServiceUnitTest(ITestOutputHelper output) {
+			this.output = output;
+			loggerFactory = LoggerFactory.Create(c => c.AddXUnit(output).SetMinimumLevel(LogLevel.Trace));
+			options = new JwtOptions() {
+				Audience = "JwtLoginServiceUnitTest",
+				Issuer = "JwtLoginServiceUnitTest",
+				SymmetricKey = "TestingSecretKeyTestingSecretKeyTestingSecretKey",
+				LoginService = new JwtLoginServiceOptions() {
+					ExpirationTime = TimeSpan.FromMinutes(5),
+					FailureDelay = TimeSpan.FromMilliseconds(400)
+				}
+			};
+			loginService = new JwtLoginService(loggerFactory.CreateLogger<JwtLoginService>(), Options.Create(options));
+		}
+
+		[Fact]
+		public async Task JwtLoginServiceIssuesValidTokenForValidCredentials() {
+			var user = new User();
+			var token = await loginService.LoginAsync(42, "UserSecret", async id => {
+				await Task.CompletedTask;
+				Assert.Equal(42, id);
+				return user;
+			}, u => {
+				Assert.Same(user, u);
+				return SecretHashing.CreateHashedSecret("UserSecret");
+			}, (u, s) => {
+				throw new XunitException("The login service should not need to rehash in this test case.");
+			});
+			var principal = tokenHandler.ValidateToken(token, new TokenValidationParameters() {
+				ValidateIssuer = true,
+				ValidateAudience = true,
+				ValidateLifetime = true,
+				ValidateIssuerSigningKey = true,
+				ValidAudience = options.Audience,
+				ValidIssuer = options.Issuer,
+				IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(options.SymmetricKey ?? throw new InvalidOperationException("Missing key.")))
+			}, out var validatedToken);
+			Assert.Equal("JwtLoginServiceUnitTest", validatedToken.Issuer);
+			Assert.Equal("42", Assert.Single(principal.Claims, c => c.Type.Equals("userid", StringComparison.OrdinalIgnoreCase)).Value);
+		}
+	}
+}
