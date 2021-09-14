384d4123d13484629cbe8c9268dafd6c3e56924b
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-14 11:16:56 +0200

    Added test case CorrectSecretIsSuccessfullyValidatedAgainstHash as unit test for secret hashing.

---
+++ SGL.Utilities.Backend.Security.Tests/SecretHashingUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SGL.Analytics.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SGL.Analytics.Backend.Security.Tests {
+	public class SecretHashingUnitTest {
+		[Fact]
+		public void CorrectSecretIsSuccessfullyValidatedAgainstHash() {
+			var secret = SecretGenerator.Instance.GenerateSecret(16);
+			var hashedSecret = SecretHashing.CreateHashedSecret(secret);
+			var (success, rehashed) = SecretHashing.VerifyHashedSecret(ref hashedSecret, secret);
+			Assert.True(success);
+			Assert.False(rehashed);
+		}
+	}
+}
