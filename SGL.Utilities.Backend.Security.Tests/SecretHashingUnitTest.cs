539fc2df26f032b29facde39f38c8ac14b569449
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-14 11:17:55 +0200

    Added test case IncorrectSecretFailsValidationAgainstHash as unit test for secret hashing.

--- SGL.Utilities.Backend.Security.Tests/SecretHashingUnitTest.cs
+++ SGL.Utilities.Backend.Security.Tests/SecretHashingUnitTest.cs
@@ -19,5 +19,14 @@ namespace SGL.Analytics.Backend.Security.Tests {
 			Assert.True(success);
 			Assert.False(rehashed);
 		}
+		[Fact]
+		public void IncorrectSecretFailsValidationAgainstHash() {
+			var secret = SecretGenerator.Instance.GenerateSecret(16);
+			var hashedSecret = SecretHashing.CreateHashedSecret(secret);
+			var modifiedSecret = new string(secret.Select(c => char.IsLetter(c) ? (char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)) : c).ToArray());
+			var (success, rehashed) = SecretHashing.VerifyHashedSecret(ref hashedSecret, modifiedSecret);
+			Assert.False(success);
+			Assert.False(rehashed);
+		}
 	}
 }
