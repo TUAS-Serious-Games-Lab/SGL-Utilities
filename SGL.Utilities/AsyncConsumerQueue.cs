7f1860a384728a1744250c9d408473350e09f65c
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-17 09:46:13 +0200

    Extracted AsyncConsumerQueue<T> to make it reusable.

---
+++ SGL.Utilities/AsyncConsumerQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Utilities {
+	public class AsyncConsumerQueue<T> {
+		private Channel<T> channel = Channel.CreateUnbounded<T>(new UnboundedChannelOptions() { AllowSynchronousContinuations = false, SingleReader = true, SingleWriter = false });
+		public void Enqueue(T item) {
+			if (!channel.Writer.TryWrite(item)) {
+				throw new InvalidOperationException("Can't enqueue to this queue object because it is already finished.");
+			}
+		}
+		public IAsyncEnumerable<T> DequeueAllAsync() {
+			return channel.Reader.ReadAllAsync();
+		}
+		public void Finish() {
+			channel.Writer.Complete();
+		}
+	}
+}
