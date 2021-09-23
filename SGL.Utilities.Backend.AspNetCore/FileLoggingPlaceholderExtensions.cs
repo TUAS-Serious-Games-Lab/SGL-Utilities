77222af068aafbb4cbe41dbe8999d8d802f76ec1
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-23 19:04:31 +0200

    Provide logging placeholders for request scope parameters in FileLoggingPlaceholderExtensions.

---
+++ SGL.Utilities.Backend.AspNetCore/FileLoggingPlaceholderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using SGL.Analytics.Utilities.Logging.FileLogging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.WebUtilities {
+	public static class FileLoggingPlaceholderExtensions {
+		public static IFileLoggingProviderBuilder AddRequestScopePlaceholders(this IFileLoggingProviderBuilder builder) {
+			builder.AddPlaceholder("RequestPath", m => {
+				var scope0 = m.Scopes.FirstOrDefault() ?? "";
+				var parts = scope0.Split(' ', ':');
+				return parts.ElementAtOrDefault(1) ?? "No_Request_Path";
+			});
+			builder.AddPlaceholder("RequestId", m => {
+				var scope0 = m.Scopes.FirstOrDefault() ?? "";
+				var parts = scope0.Split(' ', ':');
+				return parts.ElementAtOrDefault(3) ?? "No_Request_Id";
+			});
+			return builder;
+		}
+	}
+}
