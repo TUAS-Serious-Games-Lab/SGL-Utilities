daf9d6e779f517ae0f3392c32aa9474a5970d322
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-27 13:30:12 +0200

    Made RequestScopePlaceholders more robust by not assuming the request scope data to be in a specific scope index, but instead searching for them.

--- SGL.Utilities.Backend.AspNetCore/FileLoggingPlaceholderExtensions.cs
+++ SGL.Utilities.Backend.AspNetCore/FileLoggingPlaceholderExtensions.cs
@@ -9,13 +9,13 @@ namespace SGL.Analytics.Backend.WebUtilities {
 	public static class FileLoggingPlaceholderExtensions {
 		public static IFileLoggingProviderBuilder AddRequestScopePlaceholders(this IFileLoggingProviderBuilder builder) {
 			builder.AddPlaceholder("RequestPath", m => {
-				var scope0 = m.Scopes.FirstOrDefault() ?? "";
-				var parts = scope0.Split(' ', ':');
+				var scope = m.Scopes.FirstOrDefault(s => s.Contains("RequestPath")) ?? "";
+				var parts = scope.Split(' ', ':');
 				return parts.ElementAtOrDefault(1) ?? "No_Request_Path";
 			});
 			builder.AddPlaceholder("RequestId", m => {
-				var scope0 = m.Scopes.FirstOrDefault() ?? "";
-				var parts = scope0.Split(' ', ':');
+				var scope = m.Scopes.FirstOrDefault(s => s.Contains("RequestId")) ?? "";
+				var parts = scope.Split(' ', ':');
 				return parts.ElementAtOrDefault(3) ?? "No_Request_Id";
 			});
 			return builder;
