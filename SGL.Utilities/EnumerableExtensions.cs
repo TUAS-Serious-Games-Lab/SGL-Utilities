f607488a90ddb4d0664add5384d31ae24523443f
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-09-30 11:26:04 +0200

    Added an Linq-style extension method SingleOrDefaultNoExcept that doesn't throw in the 'more than one' case, but also returns the default value instead.

---
+++ SGL.Utilities/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Utilities {
+	public static class EnumerableExtensions {
+
+		/// <summary>
+		/// Behaves similarly to SingleOrDefault known from Linq, but does not throw an exception if source has more than one element, but instead returns the default value as well, like for the empty source case.
+		/// </summary>
+		/// <typeparam name="TSource">The type of the elements of the source.</typeparam>
+		/// <param name="source">A sequence of values</param>
+		/// <returns>The element of source if source has exactly one element, default(TSource) otherwise.</returns>
+		public static TSource? SingleOrDefaultNoExcept<TSource>(this IEnumerable<TSource> source) {
+			var enumerator = source.GetEnumerator();
+			if (!enumerator.MoveNext()) return default(TSource);
+			var value = enumerator.Current;
+			if (enumerator.MoveNext()) return default(TSource);
+			return value;
+		}
+	}
+}
