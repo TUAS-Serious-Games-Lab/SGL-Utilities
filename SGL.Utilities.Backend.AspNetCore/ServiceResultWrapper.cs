fed7fb0b21b31eb53e19b4bc673f846e39ec7140
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-04 09:48:50 +0200

    Implemented ServiceResultWrapper<T> to allow commands implemented as a background service to return an exit code through the DI container.

---
+++ SGL.Utilities.Backend.AspNetCore/ServiceResultWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.WebUtilities {
+
+	public class ServiceResultWrapper<T> {
+		public T Result { get; set; }
+
+		public ServiceResultWrapper(T result) {
+			Result = result;
+		}
+	}
+}
