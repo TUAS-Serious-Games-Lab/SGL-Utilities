ada4482ac247d6946ec9a8e76aa85f7cb3efccb7
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-04 10:11:30 +0200

    Extracted CommandService as an abstact base class for commands running as services.
    It runs the command implementation, provides the exit code in a ServiceResultWrapper and then stops the host.

---
+++ SGL.Utilities.Backend.AspNetCore/CommandService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.WebUtilities {
+	public abstract class CommandService : IScopedBackgroundService {
+		protected IHost host;
+		protected ServiceResultWrapper<int> exitCodeWrapper;
+
+		protected CommandService(IHost host, ServiceResultWrapper<int> exitCodeWrapper) {
+			this.host = host;
+			this.exitCodeWrapper = exitCodeWrapper;
+		}
+
+		public async Task ExecuteAsync(CancellationToken stoppingToken) {
+			await Task.Yield();
+			var result = await RunAsync(stoppingToken);
+			exitCodeWrapper.Result = result;
+			_ = host.StopAsync();
+		}
+
+		protected abstract Task<int> RunAsync(CancellationToken ct);
+	}
+}
