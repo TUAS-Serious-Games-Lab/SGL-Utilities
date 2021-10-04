f946613756fa5049fdea06c8775c74ecde0cf0d5
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-04 09:47:23 +0200

    Implemented ScopingBackgroundService<TService> and AddScopedBackgroundService<TService> to allow background services to consume scoped dependencies from the DI container.

---
+++ SGL.Utilities.Backend.AspNetCore/ScopedBackgroundService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SGL.Analytics.Backend.WebUtilities {
+	public interface IScopedBackgroundService {
+		Task ExecuteAsync(CancellationToken stoppingToken);
+	}
+
+	public class ScopingBackgroundService<TService> : BackgroundService where TService : IScopedBackgroundService {
+		IServiceProvider services;
+
+		public ScopingBackgroundService(IServiceProvider services) {
+			this.services = services;
+		}
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+			using var scope = services.CreateScope();
+			var service = scope.ServiceProvider.GetRequiredService<TService>();
+			await service.ExecuteAsync(stoppingToken);
+		}
+	}
+
+	public static class ScopedBackgroundServiceExtensions {
+		public static IServiceCollection AddScopedBackgroundService<TService>(this IServiceCollection services) where TService : class, IScopedBackgroundService {
+			services.AddScoped<TService>();
+			services.AddHostedService<ScopingBackgroundService<TService>>();
+			return services;
+		}
+	}
+}
