f2dae5ed5e00ae6322b1863806b75a4315ee2580
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-27 19:02:59 +0200

    Added API docs for FileLoggingExtensions.

--- SGL.Utilities.Logging/FileLogging/FileLoggingExtensions.cs
+++ SGL.Utilities.Logging/FileLogging/FileLoggingExtensions.cs
@@ -9,11 +9,26 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Configuration;
 
 namespace SGL.Analytics.Utilities.Logging.FileLogging {
+	/// <summary>
+	/// Provides extension methods to add an <see cref="FileLoggingProvider"/> to an <see cref="ILoggingBuilder"/>.
+	/// </summary>
 	public static class FileLoggingExtensions {
+
+		/// <summary>
+		/// Adds the <see cref="FileLoggingProvider"/> along with its config options, without customizing it using an <see cref="IFileLoggingProviderBuilder"/>.
+		/// </summary>
+		/// <param name="builder">The builder for the logging system to which the provider should be added.</param>
+		/// <returns>A reference to <c>builder</c> for chaining.</returns>
 		public static ILoggingBuilder AddFile(this ILoggingBuilder builder) {
 			return builder.AddFile(builder => { });
 		}
 
+		/// <summary>
+		/// Adds the <see cref="FileLoggingProvider"/> along with its config options, using the given <c>providerBuilder</c> to allow modifying the provider by adding custom placeholders.
+		/// </summary>
+		/// <param name="builder">The builder for the logging system to which the provider should be added.</param>
+		/// <param name="providerBuilder">A builder delegate that can be used to add custom placeholders to the provider.</param>
+		/// <returns>A reference to <c>builder</c> for chaining.</returns>
 		public static ILoggingBuilder AddFile(this ILoggingBuilder builder, Action<IFileLoggingProviderBuilder> providerBuilder) {
 			builder.AddConfiguration();
 			builder.Services.AddSingleton(providerBuilder);
