7b894d44d7c95a459eea62c46224eae5e3cbe110
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-28 13:21:32 +0200

    Added API docs for UserLogScopingExtensions.

--- SGL.Utilities.Backend.AspNetCore/UserLogScoping.cs
+++ SGL.Utilities.Backend.AspNetCore/UserLogScoping.cs
@@ -29,14 +29,37 @@ namespace SGL.Analytics.Backend.WebUtilities {
 		}
 	}
 
+	/// <summary>
+	/// Provides extension methods to add per-user log scoping.
+	/// </summary>
 	public static class UserLogScopingExtensions {
+		/// <summary>
+		/// Enables a middleware that automatically adds a <c>UserId:{ID}</c> scope to requests where a <c>userid</c> claim is persent in <see cref="HttpContext.User"/>.
+		/// </summary>
+		/// <param name="builder">The application builder where the middleware should be enabled.</param>
+		/// <returns>A reference to <c>builder</c> for chaining.</returns>
 		public static IApplicationBuilder UseUserLogScoping(this IApplicationBuilder builder) {
 			return builder.UseMiddleware<UserLogScoping>();
 		}
 
+		/// <summary>
+		/// Opens a <c>UserId:{ID}</c> logging scope.
+		/// </summary>
+		/// <typeparam name="T">The category type of the logger.</typeparam>
+		/// <param name="logger">The logger to call <see cref="LoggerExtensions.BeginScope(ILogger, string, object[])"/> on.</param>
+		/// <param name="userId">The id of the user for which to open the scope.</param>
+		/// <returns>An <see cref="IDisposable"/> object that closes the scope when disposed.</returns>
 		public static IDisposable BeginUserScope<T>(this ILogger<T> logger, string userId) {
 			return logger.BeginScope("UserId:{0}", userId);
 		}
+
+		/// <summary>
+		/// Opens a <c>UserId:{ID}</c> logging scope.
+		/// </summary>
+		/// <typeparam name="T">The category type of the logger.</typeparam>
+		/// <param name="logger">The logger to call <see cref="LoggerExtensions.BeginScope(ILogger, string, object[])"/> on.</param>
+		/// <param name="userId">The id of the user for which to open the scope.</param>
+		/// <returns>An <see cref="IDisposable"/> object that closes the scope when disposed.</returns>
 		public static IDisposable BeginUserScope<T>(this ILogger<T> logger, Guid userId) {
 			return logger.BeginScope("UserId:{0}", userId);
 		}
