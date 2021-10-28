fc85b1b5a37f0ca8dc8582373ea7fe7b1d6e3b86
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-28 13:45:40 +0200

    Added API docs for FileLoggingPlaceholderExtensions.

--- SGL.Utilities.Backend.AspNetCore/FileLoggingPlaceholderExtensions.cs
+++ SGL.Utilities.Backend.AspNetCore/FileLoggingPlaceholderExtensions.cs
@@ -6,7 +6,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.Backend.WebUtilities {
+
+	/// <summary>
+	/// Provides extension methods to add placeholders to a <see cref="FileLoggingProvider"/> that lookup webapp- / webapi-centric scope data.
+	/// </summary>
 	public static class FileLoggingPlaceholderExtensions {
+		/// <summary>
+		/// Adds the <c>RequestPath</c> and <c>RequestId</c> placeholders that lookup a <c>RequestPath:{RequestPath} RequestId:{RequestId}</c> logging scope and if found, provide the corresponding value.
+		/// If no such scope is present for a <see cref="LogMessage"/>, <c>"No_Request_Path"</c> and <c>"No_Request_Id"</c> are returned respectively instead.
+		/// </summary>
+		/// <param name="builder">The builder for the <see cref="FileLoggingProvider"/> where the placeholders shall be added.</param>
+		/// <returns>A reference to <c>builder</c> for chaining.</returns>
 		public static IFileLoggingProviderBuilder AddRequestScopePlaceholders(this IFileLoggingProviderBuilder builder) {
 			builder.AddPlaceholder("RequestPath", m => {
 				var scope = m.Scopes.FirstOrDefault(s => s.Contains("RequestPath")) ?? "";
@@ -21,6 +31,13 @@ namespace SGL.Analytics.Backend.WebUtilities {
 			return builder;
 		}
 
+		/// <summary>
+		/// Adds the <c>UserId</c> placeholder that looksup a <c>UserId:{UserId}</c> logging scope and if found, provides the id given there.
+		/// If no such scope is present for a <see cref="LogMessage"/>, <c>"No_User_Id"</c> is returned instead.
+		/// This placeholder can e.g. be used to implement per-user log files.
+		/// </summary>
+		/// <param name="builder">The builder for the <see cref="FileLoggingProvider"/> where the placeholder shall be added.</param>
+		/// <returns>A reference to <c>builder</c> for chaining.</returns>
 		public static IFileLoggingProviderBuilder AddUserIdScopePlaceholder(this IFileLoggingProviderBuilder builder) {
 			builder.AddPlaceholder("UserId", m => {
 				var scope = m.Scopes.FirstOrDefault(s => s.Contains("UserId")) ?? "";
@@ -29,6 +46,13 @@ namespace SGL.Analytics.Backend.WebUtilities {
 			});
 			return builder;
 		}
+		/// <summary>
+		/// Adds the <c>AppName</c> placeholder that looksup a <c>AppName:{AppName}</c> logging scope and if found, provides the name given there.
+		/// If no such scope is present for a <see cref="LogMessage"/>, <c>"No_App_Name"</c> is returned instead.
+		/// This placeholder can e.g. be used to implement per-app log files.
+		/// </summary>
+		/// <param name="builder">The builder for the <see cref="FileLoggingProvider"/> where the placeholder shall be added.</param>
+		/// <returns>A reference to <c>builder</c> for chaining.</returns>
 		public static IFileLoggingProviderBuilder AddAppNameScopePlaceholder(this IFileLoggingProviderBuilder builder) {
 			builder.AddPlaceholder("AppName", m => {
 				var scope = m.Scopes.FirstOrDefault(s => s.Contains("AppName")) ?? "";
