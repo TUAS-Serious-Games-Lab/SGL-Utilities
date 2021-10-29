0b289f6d378ac8f9f336935bd3fb52f8285856ad
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-29 15:06:18 +0200

    Made CommandService more general by allowing any result type.

--- SGL.Utilities.Backend/CommandService.cs
+++ SGL.Utilities.Backend/CommandService.cs
@@ -4,27 +4,30 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace SGL.Analytics.Backend.Utilities {
-	public abstract class CommandService : IScopedBackgroundService {
-		protected IHost host;
-		protected ServiceResultWrapper<int> exitCodeWrapper;
 
-		protected CommandService(IHost host, ServiceResultWrapper<int> exitCodeWrapper) {
+	public abstract class CommandService<TResult> : IScopedBackgroundService {
+		protected IHost host;
+		protected ServiceResultWrapper<TResult> resultWrapper;
+
+		protected CommandService(IHost host, ServiceResultWrapper<TResult> resultWrapper) {
 			this.host = host;
-			this.exitCodeWrapper = exitCodeWrapper;
+			this.resultWrapper = resultWrapper;
 		}
 
 		public async Task ExecuteAsync(CancellationToken stoppingToken) {
 			await Task.Yield();
 			try {
 				var result = await RunAsync(stoppingToken);
-				exitCodeWrapper.Result = result;
+				resultWrapper.Result = result;
 			}
-			catch (Exception) {
-				exitCodeWrapper.Result = 255;
+			catch (Exception ex) {
+				resultWrapper.Result = ResultForUncaughtException(ex);
 			}
 			_ = host.StopAsync();
 		}
 
-		protected abstract Task<int> RunAsync(CancellationToken ct);
+		protected abstract Task<TResult> RunAsync(CancellationToken ct);
+
+		protected abstract TResult ResultForUncaughtException(Exception ex);
 	}
 }
