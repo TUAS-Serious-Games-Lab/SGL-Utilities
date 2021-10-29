b8cafba3ffb9af894ca2d81d862bd88d9aa61809
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-10-29 13:27:02 +0200

    Added API docs for HostExtensions.

--- SGL.Utilities.Backend/HostExtensions.cs
+++ SGL.Utilities.Backend/HostExtensions.cs
@@ -11,8 +11,21 @@ using System.Threading;
 using Microsoft.Extensions.Configuration;
 
 namespace SGL.Analytics.Backend.Utilities {
+	/// <summary>
+	/// Provides extension methods for <see cref="IHost"/> to help with application startup.
+	/// </summary>
 	public static class HostExtensions {
 
+		/// <summary>
+		/// Asynchronously waits for a config value with a given key to become available by polling.
+		/// This method is intended for cases where a value, e.g. a generated secret, is expected to be provided externally at startup by some other process that must complete before the application can be started.
+		/// The value can e.g. be brought into the config system using a file with reloading active that is updated by the other process.
+		/// </summary>
+		/// <param name="host">The host of which the configuration is polled.</param>
+		/// <param name="key">The configuration key to poll.</param>
+		/// <param name="pollingInterval">The time between polls.</param>
+		/// <param name="ct">A cancellation token to allow the wating and polling operation to be cancelled.</param>
+		/// <returns></returns>
 		public static async Task WaitForConfigValueSetAsync(this IHost host, string key, TimeSpan pollingInterval, CancellationToken ct = default) {
 			var conf = host.Services.GetRequiredService<IConfiguration>();
 			if (string.IsNullOrWhiteSpace(conf.GetValue<string>(key))) {
@@ -26,6 +39,17 @@ namespace SGL.Analytics.Backend.Utilities {
 			}
 		}
 
+		/// <summary>
+		/// Asynchronously waits for database contexts of all given types to have their database available and on the expected migration version by polling
+		/// <see cref="Microsoft.EntityFrameworkCore.Infrastructure.DatabaseFacade.CanConnectAsync(CancellationToken)"/> and
+		/// <see cref="RelationalDatabaseFacadeExtensions.GetPendingMigrationsAsync(Microsoft.EntityFrameworkCore.Infrastructure.DatabaseFacade, CancellationToken)"/>.
+		/// This is intended to wait for the database server / container to be up and ready and for a migrations script to run on deployment.
+		/// </summary>
+		/// <param name="host">The host in which the database contexts are to be hosted.</param>
+		/// <param name="pollingInterval">The time between polls.</param>
+		/// <param name="ct">A cancellation token to allow the wating and polling operation to be cancelled.</param>
+		/// <param name="contextTypes">The types of the database context classes.</param>
+		/// <returns>A task that becomes ready when all databases are available.</returns>
 		public static async Task WaitForDbsReadyAsync(this IHost host, TimeSpan pollingInterval, CancellationToken ct, params Type[] contextTypes) {
 			using (var serviceScope = host.Services.CreateScope()) {
 				using (var contexts = contextTypes.Select(type => serviceScope.ServiceProvider.GetRequiredService(type)).OfType<DbContext>().ToDisposableEnumerable()) {
@@ -63,35 +87,103 @@ namespace SGL.Analytics.Backend.Utilities {
 			return (await Task.WhenAll(contexts.Select(async context => (context.GetType().Name, await context.Database.GetPendingMigrationsAsync(ct))))).Where(cm => cm.Item2.Any());
 		}
 
+		/// <summary>
+		/// Asynchronously waits for a database context of the given type to have its database available and on the expected migration version.
+		/// Acts as a convenience wrapper around <see cref="WaitForDbsReadyAsync(IHost, TimeSpan, CancellationToken, Type[])"/>.
+		/// </summary>
+		/// <typeparam name="TContext">The context class to wait for.</typeparam>
 		public static async Task WaitForDbReadyAsync<TContext>(this IHost host, TimeSpan pollingInterval, CancellationToken ct = default) where TContext : DbContext {
 			await host.WaitForDbsReadyAsync(pollingInterval, ct, typeof(TContext));
 		}
+		/// <summary>
+		/// Asynchronously waits for database contexts of the given types to have their database available and on the expected migration version.
+		/// Acts as a convenience wrapper around <see cref="WaitForDbsReadyAsync(IHost, TimeSpan, CancellationToken, Type[])"/>.
+		/// </summary>
+		/// <typeparam name="TContext1">The first context class to wait for.</typeparam>
+		/// <typeparam name="TContext2">The second context class to wait for.</typeparam>
 		public static async Task WaitForDbsReadyAsync<TContext1, TContext2>(this IHost host, TimeSpan pollingInterval, CancellationToken ct = default)
 			where TContext1 : DbContext where TContext2 : DbContext {
 			await host.WaitForDbsReadyAsync(pollingInterval, ct, typeof(TContext1), typeof(TContext2));
 		}
+		/// <summary>
+		/// Asynchronously waits for database contexts of the given types to have their database available and on the expected migration version.
+		/// Acts as a convenience wrapper around <see cref="WaitForDbsReadyAsync(IHost, TimeSpan, CancellationToken, Type[])"/>.
+		/// </summary>
+		/// <typeparam name="TContext1">The first context class to wait for.</typeparam>
+		/// <typeparam name="TContext2">The second context class to wait for.</typeparam>
+		/// <typeparam name="TContext3">The third context class to wait for.</typeparam>
 		public static async Task WaitForDbsReadyAsync<TContext1, TContext2, TContext3>(this IHost host, TimeSpan pollingInterval, CancellationToken ct = default)
 			where TContext1 : DbContext where TContext2 : DbContext where TContext3 : DbContext {
 			await host.WaitForDbsReadyAsync(pollingInterval, ct, typeof(TContext1), typeof(TContext2), typeof(TContext3));
 		}
+		/// <summary>
+		/// Asynchronously waits for database contexts of the given types to have their database available and on the expected migration version.
+		/// Acts as a convenience wrapper around <see cref="WaitForDbsReadyAsync(IHost, TimeSpan, CancellationToken, Type[])"/>.
+		/// </summary>
+		/// <typeparam name="TContext1">The first context class to wait for.</typeparam>
+		/// <typeparam name="TContext2">The second context class to wait for.</typeparam>
+		/// <typeparam name="TContext3">The third context class to wait for.</typeparam>
+		/// <typeparam name="TContext4">The fourth context class to wait for.</typeparam>
 		public static async Task WaitForDbsReadyAsync<TContext1, TContext2, TContext3, TContext4>(this IHost host, TimeSpan pollingInterval, CancellationToken ct = default)
 			where TContext1 : DbContext where TContext2 : DbContext where TContext3 : DbContext where TContext4 : DbContext {
 			await host.WaitForDbsReadyAsync(pollingInterval, ct, typeof(TContext1), typeof(TContext2), typeof(TContext3), typeof(TContext4));
 		}
+		/// <summary>
+		/// Asynchronously waits for database contexts of the given types to have their database available and on the expected migration version.
+		/// Acts as a convenience wrapper around <see cref="WaitForDbsReadyAsync(IHost, TimeSpan, CancellationToken, Type[])"/>.
+		/// </summary>
+		/// <typeparam name="TContext1">The first context class to wait for.</typeparam>
+		/// <typeparam name="TContext2">The second context class to wait for.</typeparam>
+		/// <typeparam name="TContext3">The third context class to wait for.</typeparam>
+		/// <typeparam name="TContext4">The fourth context class to wait for.</typeparam>
+		/// <typeparam name="TContext5">The fifth context class to wait for.</typeparam>
 		public static async Task WaitForDbsReadyAsync<TContext1, TContext2, TContext3, TContext4, TContext5>(this IHost host, TimeSpan pollingInterval, CancellationToken ct = default)
 			where TContext1 : DbContext where TContext2 : DbContext where TContext3 : DbContext where TContext4 : DbContext where TContext5 : DbContext {
 			await host.WaitForDbsReadyAsync(pollingInterval, ct, typeof(TContext1), typeof(TContext2), typeof(TContext3), typeof(TContext4), typeof(TContext5));
 		}
+		/// <summary>
+		/// Asynchronously waits for database contexts of the given types to have their database available and on the expected migration version.
+		/// Acts as a convenience wrapper around <see cref="WaitForDbsReadyAsync(IHost, TimeSpan, CancellationToken, Type[])"/>.
+		/// </summary>
+		/// <typeparam name="TContext1">The first context class to wait for.</typeparam>
+		/// <typeparam name="TContext2">The second context class to wait for.</typeparam>
+		/// <typeparam name="TContext3">The third context class to wait for.</typeparam>
+		/// <typeparam name="TContext4">The fourth context class to wait for.</typeparam>
+		/// <typeparam name="TContext5">The fifth context class to wait for.</typeparam>
+		/// <typeparam name="TContext6">The sixth context class to wait for.</typeparam>
 		public static async Task WaitForDbsReadyAsync<TContext1, TContext2, TContext3, TContext4, TContext5, TContext6>(this IHost host, TimeSpan pollingInterval, CancellationToken ct = default)
 			where TContext1 : DbContext where TContext2 : DbContext where TContext3 : DbContext where TContext4 : DbContext where TContext5 : DbContext where TContext6 : DbContext {
 			await host.WaitForDbsReadyAsync(pollingInterval, ct, typeof(TContext1), typeof(TContext2), typeof(TContext3), typeof(TContext4), typeof(TContext5), typeof(TContext6));
 		}
+		/// <summary>
+		/// Asynchronously waits for database contexts of the given types to have their database available and on the expected migration version.
+		/// Acts as a convenience wrapper around <see cref="WaitForDbsReadyAsync(IHost, TimeSpan, CancellationToken, Type[])"/>.
+		/// </summary>
+		/// <typeparam name="TContext1">The first context class to wait for.</typeparam>
+		/// <typeparam name="TContext2">The second context class to wait for.</typeparam>
+		/// <typeparam name="TContext3">The third context class to wait for.</typeparam>
+		/// <typeparam name="TContext4">The fourth context class to wait for.</typeparam>
+		/// <typeparam name="TContext5">The fifth context class to wait for.</typeparam>
+		/// <typeparam name="TContext6">The sixth context class to wait for.</typeparam>
+		/// <typeparam name="TContext7">The seventh context class to wait for.</typeparam>
 		public static async Task WaitForDbsReadyAsync<TContext1, TContext2, TContext3, TContext4, TContext5, TContext6, TContext7>(this IHost host, TimeSpan pollingInterval, CancellationToken ct = default)
 			where TContext1 : DbContext where TContext2 : DbContext where TContext3 : DbContext where TContext4 : DbContext where TContext5 : DbContext
 			where TContext6 : DbContext where TContext7 : DbContext {
 			await host.WaitForDbsReadyAsync(pollingInterval, ct, typeof(TContext1), typeof(TContext2), typeof(TContext3), typeof(TContext4), typeof(TContext5),
 				typeof(TContext6), typeof(TContext7));
 		}
+		/// <summary>
+		/// Asynchronously waits for database contexts of the given types to have their database available and on the expected migration version.
+		/// Acts as a convenience wrapper around <see cref="WaitForDbsReadyAsync(IHost, TimeSpan, CancellationToken, Type[])"/>.
+		/// </summary>
+		/// <typeparam name="TContext1">The first context class to wait for.</typeparam>
+		/// <typeparam name="TContext2">The second context class to wait for.</typeparam>
+		/// <typeparam name="TContext3">The third context class to wait for.</typeparam>
+		/// <typeparam name="TContext4">The fourth context class to wait for.</typeparam>
+		/// <typeparam name="TContext5">The fifth context class to wait for.</typeparam>
+		/// <typeparam name="TContext6">The sixth context class to wait for.</typeparam>
+		/// <typeparam name="TContext7">The seventh context class to wait for.</typeparam>
+		/// <typeparam name="TContext8">The eigth context class to wait for.</typeparam>
 		public static async Task WaitForDbsReadyAsync<TContext1, TContext2, TContext3, TContext4, TContext5, TContext6, TContext7, TContext8>(this IHost host, TimeSpan pollingInterval, CancellationToken ct = default)
 			where TContext1 : DbContext where TContext2 : DbContext where TContext3 : DbContext where TContext4 : DbContext where TContext5 : DbContext
 			where TContext6 : DbContext where TContext7 : DbContext where TContext8 : DbContext {
