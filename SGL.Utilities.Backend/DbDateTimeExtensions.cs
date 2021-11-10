67c836b2d2fd0943a13578c8dbec8cd38f43d703
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2021-11-10 15:20:42 +0100

    Added overload of DbDateTimeExtensions.IsStoredinUtc for nullable properties.

--- SGL.Utilities.Backend/DbDateTimeExtensions.cs
+++ SGL.Utilities.Backend/DbDateTimeExtensions.cs
@@ -24,5 +24,20 @@ namespace SGL.Utilities.Backend {
 			property.HasConversion(ev => entityToDb(ev), db => dbToEntity(db));
 			return property;
 		}
+
+		/// <summary>
+		/// Causes the property with a <see cref="DateTime"/> value to be stored in UTC representation in the database, irrespective of the representation in the mapped object.
+		/// When persisting, <see langword="null"/> values are kept as <see langword="null"/> values, values with <see cref="DateTimeKind.Utc"/> are kept as-is,
+		/// values with <see cref="DateTimeKind.Local"/> are converted using <see cref="DateTime.ToUniversalTime"/>, and values with <see cref="DateTimeKind.Unspecified"/>
+		/// are assumed to already be in UTC (using <see cref="DateTime.SpecifyKind(DateTime, DateTimeKind)"/>) for lack of a better option.
+		/// </summary>
+		/// <param name="property">The builder for the property to manipulate.</param>
+		/// <returns>A reference to <paramref name="property"/> for chaining.</returns>
+		public static PropertyBuilder<DateTime?> IsStoredInUtc(this PropertyBuilder<DateTime?> property) {
+			property.HasConversion(
+				ev => (ev != null) ? (DateTime?)entityToDb(ev.Value) : null,
+				db => (db != null) ? (DateTime?)dbToEntity(db.Value) : null);
+			return property;
+		}
 	}
 }
