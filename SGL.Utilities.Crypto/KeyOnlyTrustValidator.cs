b7998bd1fdbd1835bab8e2eb7bb26b80a4d2fe8a
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-07 13:08:12 +0100

    Implemented KeyOnlyTrustValidator to check certificates with just a list of trusted signing public keys.

---
+++ SGL.Utilities.Crypto/KeyOnlyTrustValidator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Utilities.Crypto {
+	public class KeyOnlyTrustValidator : ICertificateValidator {
+		private readonly ILogger<KeyOnlyTrustValidator> logger;
+		private readonly List<AsymmetricKeyParameter> trustedPublicKeys;
+
+		public bool CheckCertificate(X509Certificate cert) {
+			foreach (var trustedKey in trustedPublicKeys) {
+				var outcome = CertificateCheckHelper.CheckCertificate(cert, trustedKey);
+				if (outcome == CertificateCheckHelper.Outcome.OutOfValidityPeriod) {
+					logger.LogError("The certificate {subjDN} is out of it's validity period (expired or not yet valid).", cert.SubjectDN);
+					return false;
+				}
+				if (outcome == CertificateCheckHelper.Outcome.Valid) {
+					return true;
+				}
+			}
+			logger.LogError("The certificate {subjDN} could not be validated with any of the trusted public keys.", cert.SubjectDN);
+			return false;
+		}
+	}
+}
