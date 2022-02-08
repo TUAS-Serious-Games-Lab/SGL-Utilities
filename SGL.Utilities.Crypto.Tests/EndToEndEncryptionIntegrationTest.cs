c55da1d000efd0fe3628ffa49996587e7380faf4
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-08 10:59:59 +0100

    Added integration test cases for a scenario where an attacker managed to insert their own certificate into the recipient list.

--- SGL.Utilities.Crypto.Tests/EndToEndEncryptionIntegrationTest.cs
+++ SGL.Utilities.Crypto.Tests/EndToEndEncryptionIntegrationTest.cs
@@ -108,5 +108,55 @@ namespace SGL.Utilities.Crypto.Tests {
 			await TestE2EELoadingAndCorrectRoundTrip(fixture.EcPriv2Pem, expectMissingDataKey: true);
 		}
 
+		private async Task ForgedCertificatesAreRejectedAndDontGetDataKey(byte[] recipientPrivateKeyPem) {
+			KeyOnlyTrustValidator validator = new KeyOnlyTrustValidator(loggerFactory.CreateLogger<KeyOnlyTrustValidator>());
+			using var signerPemReader = new StreamReader(new MemoryStream(fixture.SignerPubPem));
+			validator.LoadPublicKeysFromReader(signerPemReader, "TestSigner.pem");
+			Assert.False(validator.CheckCertificate(fixture.RsaCertAttacker));
+			Assert.False(validator.CheckCertificate(fixture.EcCertAttacker));
+
+			var certStore = new CertificateStore(loggerFactory.CreateLogger<CertificateStore>(), validator);
+			using var certsPemReader = new StreamReader(new MemoryStream(fixture.RsaCert1Pem.Concat(fixture.EcCert1Pem).Concat(fixture.RsaCertAttackerPem).Concat(fixture.EcCertAttackerPem).ToArray()));
+			certStore.LoadCertificatesFromReader(certsPemReader, "Recipients.pem");
+			Assert.Equal(2, certStore.ListKnownCertificates().Count());
+			Assert.Equal(fixture.RsaCert1, certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.RsaKeyPair1.Public)));
+			Assert.Equal(fixture.EcCert1, certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.EcKeyPair1.Public)));
+
+			var clearTextInput1 = GenerateTestContent(1 << 20);
+			using var encryptedContent1 = new MemoryStream();
+			EncryptionInfo metadata1;
+
+			var keyEncryptor = new KeyEncryptor(certStore.ListKnownKeyIdsAndPublicKeys().ToList(), fixture.Random);
+			{
+				var dataEncryptor = new DataEncryptor(fixture.Random);
+				using var clearInputStream1 = new MemoryStream(clearTextInput1);
+				using var encStream = dataEncryptor.OpenEncryptionWriteStream(encryptedContent1);
+				await clearInputStream1.CopyToAsync(encStream);
+				metadata1 = dataEncryptor.GenerateEncryptionInfo(keyEncryptor);
+			}
+
+			Assert.Equal(DataEncryptionMode.AES_256_CCM, metadata1.DataMode);
+			Assert.Equal(2, metadata1.DataKeys.Values.Count);
+			Assert.All(metadata1.DataKeys.Values, dki => Assert.InRange(dki.EncryptedKey.Length, 32, 1024));
+
+			var privKeyStore = new PrivateKeyStore();
+			using var privKeyPemReader = new StreamReader(new MemoryStream(recipientPrivateKeyPem));
+			privKeyStore.LoadKeyPair(privKeyPemReader, fixture.PrivKeyPassword);
+			using var clearOutputStream1 = new MemoryStream();
+			var keyDecryptor = new KeyDecryptor(privKeyStore.KeyPair);
+
+			var dataDecryptor = DataDecryptor.FromEncryptionInfo(metadata1, keyDecryptor);
+			Assert.Null(dataDecryptor);
+		}
+
+		[Fact]
+		public async Task ForgedRsaCertificatesAreRejectedAndDontGetDataKey() {
+			await ForgedCertificatesAreRejectedAndDontGetDataKey(fixture.RsaPrivAttackerPem);
+		}
+
+		[Fact]
+		public async Task ForgedEcCertificatesAreRejectedAndDontGetDataKey() {
+			await ForgedCertificatesAreRejectedAndDontGetDataKey(fixture.EcPrivAttackerPem);
+		}
 	}
 }
