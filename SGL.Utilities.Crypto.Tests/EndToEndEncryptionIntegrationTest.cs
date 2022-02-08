fd792f077de87814885c9193509d77803a316e28
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-08 12:50:09 +0100

    Added test cases to verify that data keys can not be decrypted by other key pairs.

--- SGL.Utilities.Crypto.Tests/EndToEndEncryptionIntegrationTest.cs
+++ SGL.Utilities.Crypto.Tests/EndToEndEncryptionIntegrationTest.cs
@@ -108,7 +108,7 @@ namespace SGL.Utilities.Crypto.Tests {
 			await TestE2EELoadingAndCorrectRoundTrip(fixture.EcPriv2Pem, expectMissingDataKey: true);
 		}
 
-		private async Task ForgedCertificatesAreRejectedAndDontGetDataKey(byte[] recipientPrivateKeyPem) {
+		private async Task ForgedCertificatesAreRejectedAndDontGetDataKey(byte[] attackerPrivateKeyPem) {
 			KeyOnlyTrustValidator validator = new KeyOnlyTrustValidator(loggerFactory.CreateLogger<KeyOnlyTrustValidator>());
 			using var signerPemReader = new StreamReader(new MemoryStream(fixture.SignerPubPem));
 			validator.LoadPublicKeysFromReader(signerPemReader, "TestSigner.pem");
@@ -121,6 +121,8 @@ namespace SGL.Utilities.Crypto.Tests {
 			Assert.Equal(2, certStore.ListKnownCertificates().Count());
 			Assert.Equal(fixture.RsaCert1, certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.RsaKeyPair1.Public)));
 			Assert.Equal(fixture.EcCert1, certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.EcKeyPair1.Public)));
+			Assert.Null(certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.RsaKeyPairAttacker.Public)));
+			Assert.Null(certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.EcKeyPairAttacker.Public)));
 
 			var clearTextInput1 = GenerateTestContent(1 << 20);
 			using var encryptedContent1 = new MemoryStream();
@@ -140,7 +142,7 @@ namespace SGL.Utilities.Crypto.Tests {
 			Assert.All(metadata1.DataKeys.Values, dki => Assert.InRange(dki.EncryptedKey.Length, 32, 1024));
 
 			var privKeyStore = new PrivateKeyStore();
-			using var privKeyPemReader = new StreamReader(new MemoryStream(recipientPrivateKeyPem));
+			using var privKeyPemReader = new StreamReader(new MemoryStream(attackerPrivateKeyPem));
 			privKeyStore.LoadKeyPair(privKeyPemReader, fixture.PrivKeyPassword);
 			using var clearOutputStream1 = new MemoryStream();
 			var keyDecryptor = new KeyDecryptor(privKeyStore.KeyPair);
@@ -157,6 +159,58 @@ namespace SGL.Utilities.Crypto.Tests {
 		[Fact]
 		public async Task ForgedEcCertificatesAreRejectedAndDontGetDataKey() {
 			await ForgedCertificatesAreRejectedAndDontGetDataKey(fixture.EcPrivAttackerPem);
+		}
+
+		private async Task DataKeyCantBeDecryptedByOtherKeyPairs(KeyId impersonatedRecipient, byte[] attackerPrivateKeyPem) {
+			KeyOnlyTrustValidator validator = new KeyOnlyTrustValidator(loggerFactory.CreateLogger<KeyOnlyTrustValidator>());
+			using var signerPemReader = new StreamReader(new MemoryStream(fixture.SignerPubPem));
+			validator.LoadPublicKeysFromReader(signerPemReader, "TestSigner.pem");
+			Assert.False(validator.CheckCertificate(fixture.RsaCertAttacker));
+			Assert.False(validator.CheckCertificate(fixture.EcCertAttacker));
+
+			var certStore = new CertificateStore(loggerFactory.CreateLogger<CertificateStore>(), validator);
+			using var certsPemReader = new StreamReader(new MemoryStream(fixture.RsaCert1Pem.Concat(fixture.EcCert1Pem).Concat(fixture.RsaCertAttackerPem).Concat(fixture.EcCertAttackerPem).ToArray()));
+			certStore.LoadCertificatesFromReader(certsPemReader, "Recipients.pem");
+			Assert.Equal(2, certStore.ListKnownCertificates().Count());
+			Assert.Equal(fixture.RsaCert1, certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.RsaKeyPair1.Public)));
+			Assert.Equal(fixture.EcCert1, certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.EcKeyPair1.Public)));
+			Assert.Null(certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.RsaKeyPairAttacker.Public)));
+			Assert.Null(certStore.GetCertificateByKeyId(KeyId.CalculateId(fixture.EcKeyPairAttacker.Public)));
+
+			var clearTextInput1 = GenerateTestContent(1 << 20);
+			using var encryptedContent1 = new MemoryStream();
+			EncryptionInfo metadata1;
+
+			var keyEncryptor = new KeyEncryptor(certStore.ListKnownKeyIdsAndPublicKeys().ToList(), fixture.Random);
+			{
+				var dataEncryptor = new DataEncryptor(fixture.Random);
+				using var clearInputStream1 = new MemoryStream(clearTextInput1);
+				using var encStream = dataEncryptor.OpenEncryptionWriteStream(encryptedContent1);
+				await clearInputStream1.CopyToAsync(encStream);
+				metadata1 = dataEncryptor.GenerateEncryptionInfo(keyEncryptor);
+			}
+
+			Assert.Equal(DataEncryptionMode.AES_256_CCM, metadata1.DataMode);
+			Assert.Equal(2, metadata1.DataKeys.Values.Count);
+			Assert.All(metadata1.DataKeys.Values, dki => Assert.InRange(dki.EncryptedKey.Length, 32, 1024));
+
+			var privKeyStore = new PrivateKeyStore();
+			using var privKeyPemReader = new StreamReader(new MemoryStream(attackerPrivateKeyPem));
+			privKeyStore.LoadKeyPair(privKeyPemReader, fixture.PrivKeyPassword);
+			using var clearOutputStream1 = new MemoryStream();
+			var keyDecryptor = new KeyDecryptor(privKeyStore.KeyPair);
+
+			Assert.Throws<InvalidCipherTextException>(() => keyDecryptor.DecryptKey(metadata1.DataKeys[impersonatedRecipient], metadata1.SenderPublicKey));
+		}
+
+		[Fact]
+		public async Task DataKeyCantBeDecryptedByOtherRsaKeyPairs() {
+			await DataKeyCantBeDecryptedByOtherKeyPairs(KeyId.CalculateId(fixture.RsaKeyPair1.Public), fixture.RsaPrivAttackerPem);
+		}
+
+		[Fact]
+		public async Task DataKeyCantBeDecryptedByOtherEcKeyPairs() {
+			await DataKeyCantBeDecryptedByOtherKeyPairs(KeyId.CalculateId(fixture.EcKeyPair1.Public), fixture.EcPrivAttackerPem);
 		}
 	}
 }
