9d409daf3e9f4bc11c31b8325418122f5d0e1ed4
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-14 10:11:11 +0100

    Added API docs for KeyEncryptor.

--- SGL.Utilities.Crypto/KeyEncryptor.cs
+++ SGL.Utilities.Crypto/KeyEncryptor.cs
@@ -13,12 +13,26 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace SGL.Utilities.Crypto {
+	/// <summary>
+	/// Provides the functionality to encrypt data keys of data objects for a list of recipients and to generate the metadata associating recipient key ids with the recipient's encrypted copy of the data key.
+	/// </summary>
 	public class KeyEncryptor {
 		private readonly List<KeyValuePair<KeyId, AsymmetricKeyParameter>> trustedRecipients;
 		private readonly SecureRandom random;
 		private readonly bool useSharedSenderKeyPair;
 		private readonly DerObjectIdentifier? ecSharedSenderKeyPairCurveName = null;
 
+		/// <summary>
+		/// Creates a KeyEncryptor that encrypts a copy of the data key for each of the recipients who have their public key (and its id) listed in <paramref name="trustedRecipients"/>.
+		/// </summary>
+		/// <param name="trustedRecipients">The list of authorized recipient public keys that shall be able to decrypt data objects the data keys of which are encrypted using <see cref="EncryptDataKey(byte[])"/>.</param>
+		/// <param name="random">The random generator to generate sender key pairs for the <see cref="KeyEncryptionMode.ECDH_KDF2_SHA256_AES_256_CCM"/> mode, which is used for recipients that use an Elliptic Curve key pair.</param>
+		/// <param name="allowSharedSenderKeyPair">
+		/// Specifies whether the <see cref="KeyEncryptionMode.ECDH_KDF2_SHA256_AES_256_CCM"/> mode is allowed to use a shared sender key pair for all or most of the recipients with Elliptic Curve key pairs.
+		/// If this is set to true, the named curve used by the most recipient key pairs is determined and a shared sender key pair is generated that will be used for all recipients that use that curve.
+		/// The other Elliptic-Curve-using recipients will still use a specific sender key pair.
+		/// If this is set to false, all Elliptic-Curve-using recipients will get their own specific sender key pair.
+		/// </param>
 		public KeyEncryptor(List<KeyValuePair<KeyId, AsymmetricKeyParameter>> trustedRecipients, SecureRandom random, bool allowSharedSenderKeyPair = false) {
 			this.trustedRecipients = trustedRecipients;
 			this.random = random;
@@ -43,6 +57,15 @@ namespace SGL.Utilities.Crypto {
 			}
 		}
 
+		/// <summary>
+		/// Encrypts the given data key for each of the recipients specified by their public key in <see cref="KeyEncryptor(List{KeyValuePair{KeyId, AsymmetricKeyParameter}}, SecureRandom, bool)"/>.
+		/// </summary>
+		/// <param name="dataKey">The plain data key.</param>
+		/// <returns>
+		/// A <see cref="Dictionary{KeyId, DataKeyInfo}"/> containing the encrypted data keys, encryption modes, and where needed a sender public key, for each recipient, indexed by the recipient's public key <see cref="KeyId"/>.
+		/// Additionally, a shared sender public key in encoded form is returned if <c>allowSharedSenderKeyPair=true</c> was specified in <see cref="KeyEncryptor(List{KeyValuePair{KeyId, AsymmetricKeyParameter}}, SecureRandom, bool)"/>
+		/// and at least one recipient uses an Elliptic Curve key pair with a named curve. This key needs to be stored together with the encrypted data keys in the metadata of the data object that is encrypted using <paramref name="dataKey"/>.
+		/// </returns>
 		public (Dictionary<KeyId, DataKeyInfo> dataKeys, byte[]? senderPubKey) EncryptDataKey(byte[] dataKey) {
 			AsymmetricCipherKeyPair? sharedSenderKeyPair = null;
 			byte[]? encodedSharedSenderPublicKey = null;
