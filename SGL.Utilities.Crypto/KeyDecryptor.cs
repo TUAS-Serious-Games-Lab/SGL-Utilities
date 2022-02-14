442ead4596d12680c9f19fb50da5469cd8966470
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-14 13:54:17 +0100

    Added API docs for KeyDecryptor.

--- SGL.Utilities.Crypto/KeyDecryptor.cs
+++ SGL.Utilities.Crypto/KeyDecryptor.cs
@@ -4,19 +4,33 @@ using Org.BouncyCastle.Crypto.Encodings;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Modes;
 using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Security;
 using System;
 
 namespace SGL.Utilities.Crypto {
+	/// <summary>
+	/// Provides the functionality to decrypt data keys of data objects for a specific recipient that must be in the list of recipients of the data object.
+	/// The encrypted data key is taken from the data object's metadata and decrypted using an authorized private key that must be supplied to the KeyDecyrptor.
+	/// </summary>
 	public class KeyDecryptor {
 		private AsymmetricCipherKeyPair keyPair;
 		private KeyId keyId;
 
+		/// <summary>
+		/// Constructs a KeyDecryptor using the given recipient key pair.
+		/// </summary>
+		/// <param name="keyPair">The recipient key pair to use for decryption.</param>
 		public KeyDecryptor(AsymmetricCipherKeyPair keyPair) {
 			this.keyPair = keyPair;
 			keyId = KeyId.CalculateId(keyPair.Public);
 		}
 
+		/// <summary>
+		/// Looks up the data key for this KeyDecryptors key pair in the data object metadata provided in <paramref name="encryptionInfo"/> and decrypts the data key if one is found.
+		/// </summary>
+		/// <param name="encryptionInfo">The metadata for a data object, for which the recipient handled by this KeyDecryptor is an authorized recipient.</param>
+		/// <returns>The decrypted data key, or null if there is no data key </returns>
+		/// <exception cref="ArgumentException">If the found data key in <see cref="EncryptionInfo.DataKeys"/> of <paramref name="encryptionInfo"/> uses an unsupported encryption mode.</exception>
+		/// <exception cref="CryptoException">If the decryption itself fails.</exception>
 		public byte[]? DecryptKey(EncryptionInfo encryptionInfo) {
 			if (encryptionInfo.DataKeys.TryGetValue(keyId, out var dataKeyInfo)) {
 				return DecryptKey(dataKeyInfo, encryptionInfo.SenderPublicKey);
@@ -26,6 +40,17 @@ namespace SGL.Utilities.Crypto {
 			}
 		}
 
+		/// <summary>
+		/// Decrypts the data key given in <paramref name="dataKeyInfo"/> and returns the raw data key.
+		/// </summary>
+		/// <param name="dataKeyInfo">The key information containing the encrypted data key.</param>
+		/// <param name="sharedSenderPublicKey">
+		/// If <paramref name="dataKeyInfo"/> uses <see cref="KeyEncryptionMode.ECDH_KDF2_SHA256_AES_256_CCM"/> with a shared sender key, the shared public sender key must be supplied here.
+		/// It is usually taken from <see cref="EncryptionInfo.SenderPublicKey"/>.
+		/// </param>
+		/// <returns>The decrypted data key.</returns>
+		/// <exception cref="ArgumentException">If <paramref name="dataKeyInfo"/> uses an unsupported encryption mode.</exception>
+		/// <exception cref="CryptoException">If the decryption itself fails.</exception>
 		public byte[] DecryptKey(DataKeyInfo dataKeyInfo, byte[]? sharedSenderPublicKey) {
 			switch (dataKeyInfo.Mode) {
 				case KeyEncryptionMode.ECDH_KDF2_SHA256_AES_256_CCM:
