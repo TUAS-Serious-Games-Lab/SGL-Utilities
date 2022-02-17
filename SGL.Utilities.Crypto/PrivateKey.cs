bb25deec591a2e7a64d22fcebaecc2f9da4cc4e9
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-17 20:03:10 +0100

    Provide single source of truth methods for what BC key types are valid and how they map to KeyType for PublicKey and PrivateKey.

--- SGL.Utilities.Crypto/PrivateKey.cs
+++ SGL.Utilities.Crypto/PrivateKey.cs
@@ -1,21 +1,27 @@
 ﻿using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Parameters;
-using System;
 
 namespace SGL.Utilities.Crypto {
 	public class PrivateKey {
 		internal AsymmetricKeyParameter wrapped;
 
 		internal PrivateKey(AsymmetricKeyParameter wrapped) {
-			if (!wrapped.IsPrivate) throw new ArgumentException("Expecting a private key but got a public key", nameof(wrapped));
-			if (wrapped is not (RsaPrivateCrtKeyParameters or ECPrivateKeyParameters)) throw new KeyException("Unexpected key type");
+			if (!wrapped.IsPrivate) throw new KeyException("Expecting a private key but got a public key");
+			if (!IsValidWrappedType(wrapped)) throw new KeyException("Unexpected key type");
 			this.wrapped = wrapped;
 		}
+
+		internal static bool IsValidWrappedType(AsymmetricKeyParameter wrapped) => TryGetKeyType(wrapped) != null;
+		internal static KeyType? TryGetKeyType(AsymmetricKeyParameter wrapped) => wrapped switch {
+			RsaPrivateCrtKeyParameters rsa => KeyType.RSA,
+			ECPrivateKeyParameters => KeyType.EllipticCurves,
+			_ => null
+		};
 
 		public override bool Equals(object? obj) => obj is PrivateKey privateKey && wrapped.Equals(privateKey.wrapped);
 		public override int GetHashCode() => wrapped.GetHashCode();
 		public override string? ToString() => wrapped.ToString();
 
-		public KeyType Type => wrapped switch { RsaPrivateCrtKeyParameters rsa => KeyType.RSA, ECPrivateKeyParameters => KeyType.EllipticCurves, _ => throw new KeyException("Unexpected key type") };
+		public KeyType Type => TryGetKeyType(wrapped) ?? throw new KeyException("Unexpected key type");
 	}
 }
--- SGL.Utilities.Crypto/PublicKey.cs
+++ SGL.Utilities.Crypto/PublicKey.cs
@@ -1,21 +1,27 @@
 ﻿using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Parameters;
-using System;
 
 namespace SGL.Utilities.Crypto {
 	public class PublicKey {
 		internal AsymmetricKeyParameter wrapped;
 
 		internal PublicKey(AsymmetricKeyParameter wrapped) {
-			if (wrapped.IsPrivate) throw new ArgumentException("Expecting a public key but got a private key", nameof(wrapped));
-			if (wrapped is not (RsaKeyParameters or ECPublicKeyParameters)) throw new KeyException("Unexpected key type");
+			if (wrapped.IsPrivate) throw new KeyException("Expecting a public key but got a private key");
+			if (!IsValidWrappedType(wrapped)) throw new KeyException("Unexpected key type");
 			this.wrapped = wrapped;
 		}
+
+		internal static bool IsValidWrappedType(AsymmetricKeyParameter wrapped) => TryGetKeyType(wrapped) != null;
+		internal static KeyType? TryGetKeyType(AsymmetricKeyParameter wrapped) => wrapped switch {
+			RsaKeyParameters rsa => KeyType.RSA,
+			ECPublicKeyParameters => KeyType.EllipticCurves,
+			_ => null
+		};
 
 		public override bool Equals(object? obj) => obj is PublicKey publicKey && wrapped.Equals(publicKey.wrapped);
 		public override int GetHashCode() => wrapped.GetHashCode();
 		public override string? ToString() => wrapped.ToString();
 
-		public KeyType Type => wrapped switch { RsaKeyParameters rsa => KeyType.RSA, ECPublicKeyParameters => KeyType.EllipticCurves, _ => throw new KeyException("Unexpected key type") };
+		public KeyType Type => TryGetKeyType(wrapped) ?? throw new KeyException("Unexpected key type");
 	}
 }
