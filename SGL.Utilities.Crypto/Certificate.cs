0262fed96c5a21570829d4303b1d5207e3f50206
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-18 10:04:10 +0100

    Added caching for SubjectKeyIdentifier and AuthorityIdentifier getters.

--- SGL.Utilities.Crypto/Certificate.cs
+++ SGL.Utilities.Crypto/Certificate.cs
@@ -26,29 +26,37 @@ namespace SGL.Utilities.Crypto {
 
 		public KeyIdentifier? SubjectKeyIdentifier {
 			get {
-				var skid = wrapped.GetExtensionValue(X509Extensions.SubjectKeyIdentifier);
-				if (skid == null) {
-					return null;
+				if (skidCache == null) {
+					var skid = wrapped.GetExtensionValue(X509Extensions.SubjectKeyIdentifier);
+					if (skid == null) {
+						return null;
+					}
+					else {
+						skidCache = new KeyIdentifier(Org.BouncyCastle.Asn1.X509.SubjectKeyIdentifier.GetInstance(Asn1Object.FromByteArray(skid.GetOctets())));
+					}
 				}
-				else {
-					return new KeyIdentifier(Org.BouncyCastle.Asn1.X509.SubjectKeyIdentifier.GetInstance(Asn1Object.FromByteArray(skid.GetOctets())));
-				}
+				return skidCache;
 			}
 		}
+		private KeyIdentifier? skidCache = null;
 		public (KeyIdentifier? KeyIdentifier, object? Issuer, byte[]? SerialNumber)? AuthorityIdentifier {
 			get {
-				var akidEnc = wrapped.GetExtensionValue(X509Extensions.AuthorityKeyIdentifier);
-				if (akidEnc == null) {
-					return null;
+				if (akidCache == null) {
+					var akidEnc = wrapped.GetExtensionValue(X509Extensions.AuthorityKeyIdentifier);
+					if (akidEnc == null) {
+						return null;
+					}
+					else {
+						var akid = Org.BouncyCastle.Asn1.X509.AuthorityKeyIdentifier.GetInstance(Asn1Object.FromByteArray(akidEnc.GetOctets()));
+						var akidRaw = akid?.GetKeyIdentifier();
+						var keyIdent = akidRaw != null ? new KeyIdentifier(new SubjectKeyIdentifier(akidRaw)) : null;
+						akidCache = (keyIdent, akid?.AuthorityCertIssuer, akid?.AuthorityCertSerialNumber?.ToByteArrayUnsigned());
+					}
 				}
-				else {
-					var akid = Org.BouncyCastle.Asn1.X509.AuthorityKeyIdentifier.GetInstance(Asn1Object.FromByteArray(akidEnc.GetOctets()));
-					var akidRaw = akid?.GetKeyIdentifier();
-					var keyIdent = akidRaw != null ? new KeyIdentifier(new SubjectKeyIdentifier(akidRaw)) : null;
-					return (keyIdent, akid?.AuthorityCertIssuer, akid?.AuthorityCertSerialNumber?.ToByteArrayUnsigned());
-				}
+				return akidCache;
 			}
 		}
+		private (KeyIdentifier? KeyIdentifier, object? Issuer, byte[]? SerialNumber)? akidCache = null;
 
 		public static Certificate LoadOneFromPem(TextReader reader, Func<char[]> passwordGetter) => PemHelper.LoadCertificate(reader);
 		public static IEnumerable<Certificate> LoadAllFromPem(TextReader reader, Func<char[]> passwordGetter) => PemHelper.LoadCertificates(reader);
