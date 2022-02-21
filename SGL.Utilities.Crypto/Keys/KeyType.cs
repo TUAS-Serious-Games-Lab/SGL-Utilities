a3d03403cd16fd0e208c848cc2d58a4ba0d33bcb
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-21 18:06:15 +0100

    Added API docs for KeyType.

--- SGL.Utilities.Crypto/Keys/KeyType.cs
+++ SGL.Utilities.Crypto/Keys/KeyType.cs
@@ -1,3 +1,15 @@
 ﻿namespace SGL.Utilities.Crypto.Keys {
-	public enum KeyType { RSA, EllipticCurves }
+	/// <summary>
+	/// Represents a type of a <see cref="KeyPair"/>, <see cref="PrivateKey"/>, or <see cref="PublicKey"/>.
+	/// </summary>
+	public enum KeyType {
+		/// <summary>
+		/// The key (pair) is for the RSA algorithm.
+		/// </summary>
+		RSA,
+		/// <summary>
+		/// The key (pair) is for Elliptic Curve Cryptography.
+		/// </summary>
+		EllipticCurves
+	}
 }
