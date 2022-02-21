f6412fd90b56fa6e1fb48706b2a2b5c90f199535
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-21 19:57:53 +0100

    Added extension package SGL.Utilities.Crypto.EntityFrameworkCore.

---
+++ SGL.Utilities.Crypto.EntityFrameworkCore/KeyIdConversionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Utilities.Crypto.EntityFrameworkCore {
+	public static class KeyIdConversionExtensions {
+	}
+}
