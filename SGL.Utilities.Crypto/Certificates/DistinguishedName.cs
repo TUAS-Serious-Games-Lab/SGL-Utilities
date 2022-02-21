3c9d3e97aeb89fb9d5f8b8623ac6382bcad6b548
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-21 12:35:10 +0100

    Added DistinguishedName.EnumerateComponents().

--- SGL.Utilities.Crypto/Certificates/DistinguishedName.cs
+++ SGL.Utilities.Crypto/Certificates/DistinguishedName.cs
@@ -1,4 +1,5 @@
-﻿using Org.BouncyCastle.Asn1.X509;
+﻿using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.X509;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -17,5 +18,10 @@ namespace SGL.Utilities.Crypto.Certificates {
 		public override bool Equals(object? obj) => obj is DistinguishedName name && wrapped.Equals(name.wrapped);
 		public override int GetHashCode() => wrapped.GetHashCode();
 		public override string? ToString() => wrapped.ToString();
+
+		public IEnumerable<KeyValuePair<string, string>> EnumerateComponents() {
+			var keys = wrapped.GetOidList().Cast<DerObjectIdentifier>().Select(oid => (string?)X509Name.DefaultSymbols[oid] ?? oid.Id);
+			return keys.Zip(wrapped.GetValueList().Cast<string>(), (k, v) => new KeyValuePair<string, string>(k, v));
+		}
 	}
 }
