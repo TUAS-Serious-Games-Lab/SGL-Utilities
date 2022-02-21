1c9a7f3e07d774407d0bdde5abba0ddfd709d467
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-02-21 18:08:07 +0100

    Added API docs for PublicKey.

--- SGL.Utilities.Crypto/Keys/PublicKey.cs
+++ SGL.Utilities.Crypto/Keys/PublicKey.cs
@@ -5,6 +5,11 @@ using System.Collections.Generic;
 using System.IO;
 
 namespace SGL.Utilities.Crypto.Keys {
+	/// <summary>
+	/// Represents the public key of a <see cref="KeyPair"/>.
+	/// This type of key can be shared openly an is used to encrypt data such that it can only be decrypted by the matching private key.
+	/// Public keys are also used to verify cryptographic signatures made using the matching private key.
+	/// </summary>
 	public class PublicKey {
 		internal AsymmetricKeyParameter wrapped;
 
@@ -21,13 +26,35 @@ namespace SGL.Utilities.Crypto.Keys {
 			_ => null
 		};
 
+		/// <inheritdoc/>
 		public override bool Equals(object? obj) => obj is PublicKey publicKey && wrapped.Equals(publicKey.wrapped);
+		/// <inheritdoc/>
 		public override int GetHashCode() => wrapped.GetHashCode();
+		/// <inheritdoc/>
 		public override string? ToString() => wrapped.ToString();
 
+		/// <summary>
+		/// Returns the type of the key.
+		/// </summary>
 		public KeyType Type => TryGetKeyType(wrapped) ?? throw new KeyException("Unexpected key type");
+		/// <summary>
+		/// Loads one public key from the PEM-encoded data in <paramref name="reader"/>.
+		/// If a full key pair is read from the PEM data, its public key will be used.
+		/// </summary>
+		/// <param name="reader">A <see cref="TextReader"/> containing at least one PEM-encoded key pair or public key.</param>
+		/// <returns>The loaded public key.</returns>
 		public static PublicKey LoadOneFromPem(TextReader reader) => PemHelper.LoadPublicKey(reader);
+		/// <summary>
+		/// Loads all public keys from the PEM-encoded data in <paramref name="reader"/>.
+		/// If a full key pair is read from the PEM data, its public key will be used.
+		/// </summary>
+		/// <param name="reader">A <see cref="TextReader"/> containing PEM-encoded public keys.</param>
+		/// <returns>An <see cref="IEnumerable{T}"/> over all public keys loaded from <paramref name="reader"/>.</returns>
 		public static IEnumerable<PublicKey> LoadAllFromPem(TextReader reader) => PemHelper.LoadPublicKeys(reader);
+		/// <summary>
+		/// Writes the public key to <paramref name="writer"/> in PEM-encoded form.
+		/// </summary>
+		/// <param name="writer">The writer to which the public key should be written.</param>
 		public void StoreToPem(TextWriter writer) => PemHelper.Write(writer, this);
 	}
 }
