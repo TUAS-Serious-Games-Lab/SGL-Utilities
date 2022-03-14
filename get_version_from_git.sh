343ad7e15b031afd1eb1aeef7b83697c62ac176d
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-14 18:09:06 +0100

    Fixed get_version_from_git.sh to also work when the current checkout is not describable by the _latest_ tag, but by a previous tag.

--- get_version_from_git.sh
+++ get_version_from_git.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-git tag --sort=committerdate | grep -P '^\d+(\.\d+)*$' | tail -n1 | xargs git describe --tags --match
+# Scans backward through commit date ordered tag list to find a tag based upon which the current version can be described
+TAGS=$(git tag --sort=committerdate | grep -P '^\d+(\.\d+)*$' | tac)
+for TAG in $TAGS; do
+	git describe --tags --match $TAG 2>/dev/null && exit 0
+done
+# If no tag worked to describe the version, error out
+>&2 echo "Error: Couldn't find version from tags!"
+exit 1
