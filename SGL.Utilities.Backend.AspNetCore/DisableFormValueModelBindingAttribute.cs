16ef6cf23bffb300b039f9e63c5496321c00b6cf
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-21 19:37:02 +0100

    Added DisableFormValueModelBindingAttribute.

---
+++ SGL.Utilities.Backend.AspNetCore/DisableFormValueModelBindingAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+
+namespace SGL.Utilities.Backend.AspNetCore {
+	/// <summary>
+	/// Disables the ASP.Net Core form value model binding to allow other components to read the body stream directly,
+	/// as specified here: https://docs.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads?view=aspnetcore-5.0#upload-large-files-with-streaming
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	public class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter {
+		/// <inheritdoc/>
+		public void OnResourceExecuting(ResourceExecutingContext context) {
+			var factories = context.ValueProviderFactories;
+			factories.RemoveType<FormValueProviderFactory>();
+			factories.RemoveType<FormFileValueProviderFactory>();
+			factories.RemoveType<JQueryFormValueProviderFactory>();
+		}
+
+		/// <inheritdoc/>
+		public void OnResourceExecuted(ResourceExecutedContext context) { }
+	}
+}
