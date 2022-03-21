fd7debe5926e4af6c7635e105dda3782c8161ae0
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-21 13:10:33 +0100

    Added PemInputFormatter to MvcOptionsPemFormatterExtensions.AddPemFormatters.

--- SGL.Utilities.Crypto.AspNetCore/MvcOptionsPemFormatterExtensions.cs
+++ SGL.Utilities.Crypto.AspNetCore/MvcOptionsPemFormatterExtensions.cs
@@ -11,11 +11,12 @@ namespace SGL.Utilities.Crypto.AspNetCore {
 	/// </summary>
 	public static class MvcOptionsPemFormatterExtensions {
 		/// <summary>
-		/// Adds an ASP.Net Core formatter for the <c>application/x-pem-file</c> content type.
+		/// Adds ASP.Net Core formatters (input and output) for the <c>application/x-pem-file</c> content type.
 		/// </summary>
-		/// <param name="options">The <see cref="MvcOptions"/> to add the formatter to.</param>
+		/// <param name="options">The <see cref="MvcOptions"/> to add the formatters to.</param>
 		/// <returns>A reference to <paramref name="options"/> for chaining.</returns>
 		public static MvcOptions AddPemFormatters(this MvcOptions options) {
+			options.InputFormatters.Insert(0, new PemInputFormatter());
 			options.OutputFormatters.Insert(0, new PemOutputFormatter());
 			return options;
 		}
