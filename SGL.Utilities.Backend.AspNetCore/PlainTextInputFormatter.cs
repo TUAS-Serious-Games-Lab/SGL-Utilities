503a78f7039154377758d6ded4a06077d2434cba
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-22 15:54:12 +0100

    Added API docs for PlainTextInputFormatter.

--- SGL.Utilities.Backend.AspNetCore/PlainTextInputFormatter.cs
+++ SGL.Utilities.Backend.AspNetCore/PlainTextInputFormatter.cs
@@ -9,7 +9,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Utilities.Backend.AspNetCore {
+	/// <summary>
+	/// Provides support for reading a request body of context type <c>text/plain</c> into a model of type <see cref="string"/>.
+	/// </summary>
 	public class PlainTextInputFormatter : TextInputFormatter {
+		/// <summary>
+		/// Initializes the formatter.
+		/// </summary>
 		public PlainTextInputFormatter() {
 			SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/plain"));
 			SupportedEncodings.Add(Encoding.UTF8);
@@ -17,6 +23,20 @@ namespace SGL.Utilities.Backend.AspNetCore {
 			SupportedEncodings.Add(Encoding.ASCII);
 		}
 
+		/// <summary>
+		/// Asynchronously reads a <see cref="string"/> model value from the request body of <paramref name="context"/>.
+		/// </summary>
+		/// <param name="context">The context to operate on.</param>
+		/// <param name="encoding">The text encoding to use.</param>
+		/// <returns>
+		/// A task object representing the asynchronous operation, wrapping the following:
+		/// <list type="bullet">
+		/// <item><term><see cref="InputFormatterResult.Success(object)"/></term><description>
+		/// If the requested object was successfully read. Contains the value read from the body.
+		/// </description></item>
+		/// <item><term><see cref="InputFormatterResult.Failure"/></term><description>Otherwise, i.e. when the body contained invalid data.</description></item>
+		/// </list>
+		/// </returns>
 		public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding) {
 			try {
 				using var reader = context.ReaderFactory(context.HttpContext.Request.Body, encoding);
@@ -33,6 +53,12 @@ namespace SGL.Utilities.Backend.AspNetCore {
 			}
 		}
 
+		/// <summary>
+		/// Returns a bool indicating whether the given type can be formatted by this formatter.
+		/// As this formatter only handles <see cref="string"/>s, this method checks whether <paramref name="type"/> is <c>typeof(string)</c>.
+		/// </summary>
+		/// <param name="type">The type to check.</param>
+		/// <returns>A bool indicating whether the given type can be formatted by this formatter.</returns>
 		protected override bool CanReadType(Type type) {
 			return type == typeof(string);
 		}
