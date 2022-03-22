bc162fb8710bc4b7696f7cdec73378b5c52987c2
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-22 12:15:37 +0100

    Added API docs for ContentType handling in FromMultipartSectionAttribute and MultipartSectionModelBinder.

--- SGL.Utilities.Backend.AspNetCore/MultipartSectionModelBinder.cs
+++ SGL.Utilities.Backend.AspNetCore/MultipartSectionModelBinder.cs
@@ -25,10 +25,16 @@ namespace SGL.Utilities.Backend.AspNetCore {
 	/// Implements ASP.Net Core model binding for scenarios where a controller method parameter should consume a whole <c>multipart/formdata</c> request body section in a manner similar to how
 	/// parameters can normally consume the request body, e.g. as a JSON object.
 	/// The parameter that should consume a request body section need to be marked with <see cref="FromMultipartSectionAttribute"/>.
+	///
 	/// Additionally, as the <see cref="MultipartSectionModelBinder"/> needs to read through the reuqest body, it is incompatible with the model binders for form values.
 	/// Thus these need to be disabled using <see cref="DisableFormValueModelBindingAttribute"/> for the method.
+	///
 	/// Each parameter is bound to a section with a name that matches the <see cref="ModelBinderAttribute.Name"/> of its <see cref="FromMultipartSectionAttribute"/>, if that is set.
 	/// Otherwise, the name of the parameter itself is used. In both cases the match is done case-insensitive.
+	///
+	/// If <see cref="FromMultipartSectionAttribute.ContentType"/> is set, a matching section also needs to have the content type specified by this to be bound to the marked parameter.
+	/// For <see cref="FromMultipartSectionAttribute.ContentType"/> to be available to this model binder, a <see cref="FromMultipartSectionMetadataProvider"/> needs to be registered, usually done using <see cref="MvcOptionsMultipartSectionMetadataExtensions.AddMultipartSectionMetadata(MvcOptions)"/>.
+	///
 	/// Obtaining the value from the body content is done using the <see cref="IInputFormatter"/>s registered in <see cref="MvcOptions"/>.
 	/// </summary>
 	public class MultipartSectionModelBinder : IModelBinder {
@@ -135,10 +141,22 @@ namespace SGL.Utilities.Backend.AspNetCore {
 		/// </summary>
 		public FromMultipartSectionAttribute() : base(typeof(MultipartSectionModelBinder)) { }
 
+		/// <summary>
+		/// If set, constraints model binding for the marked parameter to multipart sections with the specified content type.
+		/// For this to be passed through to the model binder, a <see cref="FromMultipartSectionMetadataProvider"/> needs to be registered, usually done using <see cref="MvcOptionsMultipartSectionMetadataExtensions.AddMultipartSectionMetadata(MvcOptions)"/>.
+		/// </summary>
 		public string? ContentType { get; init; } = null;
 	}
 
+	/// <summary>
+	/// Provides the <see cref="FromMultipartSectionAttribute"/> object in <see cref="DisplayMetadata.AdditionalValues"/> for model binding contexts on models marked with that attribute.
+	/// This is needed to pass the <see cref="FromMultipartSectionAttribute.ContentType"/> to <see cref="MultipartSectionModelBinder"/>.
+	/// </summary>
 	public class FromMultipartSectionMetadataProvider : IDisplayMetadataProvider {
+		/// <summary>
+		/// If a model is marked with <see cref="FromMultipartSectionAttribute"/>, provides the attribute object in <see cref="DisplayMetadata.AdditionalValues"/> of <paramref name="context"/> under the key <c>typeof(FromMultipartSectionAttribute)</c>.
+		/// </summary>
+		/// <param name="context">The context to work on.</param>
 		public void CreateDisplayMetadata(DisplayMetadataProviderContext context) {
 			var attribute = context.Attributes.OfType<FromMultipartSectionAttribute>().FirstOrDefault();
 			if (attribute != null) {
@@ -147,7 +165,15 @@ namespace SGL.Utilities.Backend.AspNetCore {
 		}
 	}
 
+	/// <summary>
+	/// Provides the <see cref="AddMultipartSectionMetadata(MvcOptions)"/> extension method.
+	/// </summary>
 	public static class MvcOptionsMultipartSectionMetadataExtensions {
+		/// <summary>
+		/// Registers <see cref="FromMultipartSectionMetadataProvider"/> in <paramref name="options"/>.
+		/// </summary>
+		/// <param name="options">The options object to modify.</param>
+		/// <returns>A reference to <paramref name="options"/> for chaining.</returns>
 		public static MvcOptions AddMultipartSectionMetadata(this MvcOptions options) {
 			options.ModelMetadataDetailsProviders.Add(new FromMultipartSectionMetadataProvider());
 			return options;
