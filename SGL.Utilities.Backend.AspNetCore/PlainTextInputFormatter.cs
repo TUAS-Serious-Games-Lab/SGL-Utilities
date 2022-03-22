3733bf34fafe84f3f31cb310babae3febd8aa759
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-22 12:44:10 +0100

    Implemented PlainTextInputFormatter.

---
+++ SGL.Utilities.Backend.AspNetCore/PlainTextInputFormatter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Utilities.Backend.AspNetCore {
+	public class PlainTextInputFormatter : TextInputFormatter {
+		public PlainTextInputFormatter() {
+			SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("text/plain"));
+			SupportedEncodings.Add(Encoding.UTF8);
+			SupportedEncodings.Add(Encoding.Unicode);
+			SupportedEncodings.Add(Encoding.ASCII);
+		}
+
+		public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context, Encoding encoding) {
+			var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<PlainTextInputFormatter>>();
+			try {
+				using var reader = context.ReaderFactory(context.HttpContext.Request.Body, encoding);
+				var content = await reader.ReadToEndAsync();
+				return InputFormatterResult.Success(content);
+			}
+			catch (OperationCanceledException) {
+				throw;
+			}
+			catch (Exception ex) {
+				logger.LogError(ex, "Reading request body failed due to exception.");
+				return InputFormatterResult.Failure();
+			}
+		}
+
+		protected override bool CanReadType(Type type) {
+			return type == typeof(string);
+		}
+	}
+}
