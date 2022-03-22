f62f36de18f693ac3a76e7fcc72151b47eae3c2f
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-22 15:56:42 +0100

    Added API docs for MvcOptionsRawFormattersExtensions.

--- SGL.Utilities.Backend.AspNetCore/MvcOptionsRawFormattersExtensions.cs
+++ SGL.Utilities.Backend.AspNetCore/MvcOptionsRawFormattersExtensions.cs
@@ -6,12 +6,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Utilities.Backend.AspNetCore {
+	/// <summary>
+	/// Provides the extension methods <see cref="AddRawBytesFormatters(MvcOptions, int)"/> and <see cref="AddPlainTextInputFormatter(MvcOptions)"/>.
+	/// </summary>
 	public static class MvcOptionsRawFormattersExtensions {
+		/// <summary>
+		/// Registers <see cref="RawBytesInputFormatter"/> and <see cref="RawBytesOutputFormatter"/> in <paramref name="options"/>.
+		/// </summary>
+		/// <param name="options">The options to add the registrations to.</param>
+		/// <param name="outputFormatterBatchSize">The <see cref="RawBytesOutputFormatter.BatchSize"/> to set.</param>
+		/// <returns>A reference to <paramref name="options"/> for chaining.</returns>
 		public static MvcOptions AddRawBytesFormatters(this MvcOptions options, int outputFormatterBatchSize = 1024 * 1024) {
 			options.InputFormatters.Insert(0, new RawBytesInputFormatter());
 			options.OutputFormatters.Insert(0, new RawBytesOutputFormatter() { BatchSize = outputFormatterBatchSize });
 			return options;
 		}
+		/// <summary>
+		/// Registers <see cref="PlainTextInputFormatter"/> in <paramref name="options"/>.
+		/// </summary>
+		/// <param name="options">The options to add the registration to.</param>
+		/// <returns>A reference to <paramref name="options"/> for chaining.</returns>
 		public static MvcOptions AddPlainTextInputFormatter(this MvcOptions options) {
 			options.InputFormatters.Insert(0, new PlainTextInputFormatter());
 			return options;
