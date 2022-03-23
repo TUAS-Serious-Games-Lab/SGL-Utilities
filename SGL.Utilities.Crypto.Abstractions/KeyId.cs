7429bddac692c89cad50e7e55a60c40bdff1c610
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-23 10:55:03 +0100

    Moved KeyId to SGL.Utilities.Crypto.Abstractions.

--- SGL.Utilities.Crypto.Abstractions/KeyId.cs
+++ SGL.Utilities.Crypto.Abstractions/KeyId.cs
@@ -1,6 +1,4 @@
-﻿using Org.BouncyCastle.Crypto.Digests;
-using Org.BouncyCastle.Crypto.Parameters;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
