123d062c2bbdc06a0b17d5b0fabf002379d25efc
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-25 09:26:14 +0100

    Implemented extension method KeyValuePropertiesBetween for ModelBuilder for creating the model for a key-value-properties construct.

---
+++ SGL.Utilities.Backend/KeyValueProperties/ModelBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Utilities.Backend.KeyValueProperties {
+	public static class ModelBuilderExtensions {
+		public static ModelBuilder KeyValuePropertiesBetween<TDefinitionOwner, TInstanceOwner, TDefinition, TInstance>(this ModelBuilder modelBuilder,
+				EntityTypeBuilder<TDefinitionOwner> definitionOwner, EntityTypeBuilder<TInstanceOwner> instanceOwner,
+				Expression<Func<TDefinitionOwner, IEnumerable<TDefinition>>> definitionExpression, Expression<Func<TInstanceOwner, IEnumerable<TInstance>>> instanceExpression, int nameLength = 128)
+				where TDefinitionOwner : class where TInstanceOwner : class where TDefinition : PropertyDefinitionBase<TDefinitionOwner> where TInstance : PropertyInstanceBase<TInstanceOwner, TDefinition> {
+			modelBuilder.Ignore<PropertyDefinitionBase>();
+			modelBuilder.Ignore<PropertyDefinitionBase<TDefinitionOwner>>();
+			modelBuilder.Ignore<PropertyInstanceBase<TInstanceOwner, TDefinition>>();
+			definitionOwner.OwnsMany(definitionExpression, def => {
+				var ownership = def.WithOwner(d => d.Owner);
+				var uniqueOwnerAndNameIndexPropNames = ownership.Metadata.Properties.Select(p => p.Name).Append(nameof(PropertyDefinitionBase.Name)).ToArray();
+				def.HasIndex(uniqueOwnerAndNameIndexPropNames).IsUnique();
+				def.Property(d => d.Name).HasMaxLength(nameLength);
+			});
+			definitionOwner.Navigation(definitionExpression).AutoInclude(false);
+			instanceOwner.OwnsMany(instanceExpression, inst => {
+				var ownership = inst.WithOwner(pi => pi.Owner);
+				var defFK = inst.HasOne(pi => pi.Definition).WithMany();
+				var uniqueDefAndOwnerIndexPropNames = defFK.Metadata.Properties.Concat(ownership.Metadata.Properties).Select(p => p.Name).ToArray();
+				inst.HasIndex(uniqueDefAndOwnerIndexPropNames).IsUnique();
+				inst.Property(pi => pi.DateTimeValue).IsStoredInUtc();
+				inst.Ignore(pi => pi.Value);
+			});
+			instanceOwner.Navigation(instanceExpression).AutoInclude(false);
+			return modelBuilder;
+		}
+	}
+}
