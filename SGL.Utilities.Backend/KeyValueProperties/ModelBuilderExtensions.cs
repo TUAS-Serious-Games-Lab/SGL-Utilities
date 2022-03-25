14e89c6744c49261bc5a9d5bf4523aab2068bf24
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-25 14:50:24 +0100

    Added API docs for ModelBuilderExtensions.

--- SGL.Utilities.Backend/KeyValueProperties/ModelBuilderExtensions.cs
+++ SGL.Utilities.Backend/KeyValueProperties/ModelBuilderExtensions.cs
@@ -8,7 +8,28 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Utilities.Backend.KeyValueProperties {
+	/// <summary>
+	/// Provides the extension method
+	/// <see cref="KeyValuePropertiesBetween{TDefinitionOwner, TInstanceOwner, TDefinition, TInstance}(ModelBuilder, EntityTypeBuilder{TDefinitionOwner}, EntityTypeBuilder{TInstanceOwner},
+	/// Expression{Func{TDefinitionOwner, IEnumerable{TDefinition}}}, Expression{Func{TInstanceOwner, IEnumerable{TInstance}}}, int)"/>.
+	/// </summary>
 	public static class ModelBuilderExtensions {
+		/// <summary>
+		/// Sets up key-value properties between the related entity types <typeparamref name="TDefinitionOwner"/> and <typeparamref name="TInstanceOwner"/> with
+		/// <typeparamref name="TDefinitionOwner"/> holding property definitions of type <typeparamref name="TDefinition"/> and
+		/// <typeparamref name="TInstanceOwner"/> holding property instances of type <typeparamref name="TInstanceOwner"/>.
+		/// </summary>
+		/// <typeparam name="TDefinitionOwner">The entity type holding the property definitions.</typeparam>
+		/// <typeparam name="TInstanceOwner">The entity type holding the property instances.</typeparam>
+		/// <typeparam name="TDefinition">The weak entity type representing a property definition, derived from <see cref="PropertyDefinitionBase{TDefinitionOwner}"/>.</typeparam>
+		/// <typeparam name="TInstance">The wead entity type representing a property instance, derived from <see cref="PropertyInstanceBase{TInstanceOwner, TDefinition}"/>.</typeparam>
+		/// <param name="modelBuilder">The <see cref="ModelBuilder"/> with which to set up the model for the properties.</param>
+		/// <param name="definitionOwner">The entity type builder for the entity holding the property definitions.</param>
+		/// <param name="instanceOwner">The entity type builder for the entity holding the property instances.</param>
+		/// <param name="definitionExpression">The expression identifying the collection of property definition within <typeparamref name="TDefinitionOwner"/>.</param>
+		/// <param name="instanceExpression">The expression identifying the collection of property instances within <typeparamref name="TInstanceOwner"/>.</param>
+		/// <param name="nameLength">The maximum length of the property names to set up.</param>
+		/// <returns>A reference to <paramref name="modelBuilder"/> for chaining.</returns>
 		public static ModelBuilder KeyValuePropertiesBetween<TDefinitionOwner, TInstanceOwner, TDefinition, TInstance>(this ModelBuilder modelBuilder,
 				EntityTypeBuilder<TDefinitionOwner> definitionOwner, EntityTypeBuilder<TInstanceOwner> instanceOwner,
 				Expression<Func<TDefinitionOwner, IEnumerable<TDefinition>>> definitionExpression, Expression<Func<TInstanceOwner, IEnumerable<TInstance>>> instanceExpression, int nameLength = 128)
