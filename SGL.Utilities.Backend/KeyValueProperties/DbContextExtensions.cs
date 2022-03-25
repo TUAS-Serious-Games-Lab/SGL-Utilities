0d5f324864df4a96c513f890054916af90ed42d4
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-03-25 10:45:38 +0100

    Provided LoadKeyValuePropertiesAsync extension methods for DbContext.

---
+++ SGL.Utilities.Backend/KeyValueProperties/DbContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Utilities.Backend.KeyValueProperties {
+	public static class DbContextExtensions {
+		public static Task LoadKeyValuePropertiesAsync<TInstanceOwner, TPropInstance, TDefinition>(this DbContext context, TInstanceOwner owner,
+				Expression<Func<TInstanceOwner, IEnumerable<TPropInstance>>> propertyInstancesExpression)
+				where TInstanceOwner : class
+				where TDefinition : PropertyDefinitionBase
+				where TPropInstance : PropertyInstanceBase<TInstanceOwner, TDefinition> {
+			return context.Entry(owner).Collection(propertyInstancesExpression).LoadAsync();
+		}
+		public static Task LoadKeyValuePropertiesAsync<TDefinitionOwner, TDefinition>(this DbContext context, TDefinitionOwner owner,
+				Expression<Func<TDefinitionOwner, IEnumerable<TDefinition>>> propertyDefinitionsExpression)
+				where TDefinitionOwner : class
+				where TDefinition : PropertyDefinitionBase<TDefinitionOwner> {
+			return context.Entry(owner).Collection(propertyDefinitionsExpression).LoadAsync();
+		}
+	}
+}
