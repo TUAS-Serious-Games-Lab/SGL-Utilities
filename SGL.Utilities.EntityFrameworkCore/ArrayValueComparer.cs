6fc8172319cc2d634d9bfea3e60d4684e82c2c21
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-05-17 17:53:08 +0200

    Provide ArrayValueComparer<T> in SGL.Utilities.EntityFrameworkCore.

---
+++ SGL.Utilities.EntityFrameworkCore/ArrayValueComparer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Linq;
+
+namespace SGL.Utilities.EntityFrameworkCore {
+	/// <summary>
+	/// Implements a recursive <see cref="ValueComparer{T}"/> for arrays of element type <typeparamref name="T"/>.
+	/// </summary>
+	public class ArrayValueComparer<T> : ValueComparer<T[]> {
+		/// <summary>
+		/// Constructs an <see cref="ArrayValueComparer{T}"/> that uses the given comparer for the elements.
+		/// </summary>
+		/// <param name="elementValueComparer">The <see cref="ValueComparer{T}"/> to use on the elements.</param>
+		public ArrayValueComparer(ValueComparer elementValueComparer) :
+			base(
+				(a, b) => checkEqual(elementValueComparer, a, b),
+				(e) => computeHashCode(elementValueComparer, e),
+				(e) => makeSnapshot(elementValueComparer, e)) { }
+
+		private static bool checkEqual(ValueComparer elementValueComparer, T[] a, T[] b) {
+			if (a.Length != b.Length) return false;
+			for (int i = 0; i < a.Length; ++i) {
+				if (elementValueComparer.Equals(a[i], b[i])) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		private static int computeHashCode(ValueComparer elementValueComparer, T[] e) {
+			return e.Aggregate(104179, (h, e) => HashCode.Combine(h, e));
+		}
+
+		private static T[] makeSnapshot(ValueComparer elementValueComparer, T[] e) {
+			var res = new T[e.Length];
+			for (int i = 0; i < e.Length; ++i) {
+				res[i] = (T)elementValueComparer.Snapshot(e[i]);
+			}
+			return res;
+		}
+	}
+}
