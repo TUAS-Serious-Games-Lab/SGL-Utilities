adaea2abc5747165203e0421fda06b31dab271c2
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-13 14:57:43 +0200

    Added missing API docs.

--- SGL.Utilities.Crypto/PemObjectReader.cs
+++ SGL.Utilities.Crypto/PemObjectReader.cs
@@ -15,16 +15,39 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Utilities.Crypto {
+
+	/// <summary>
+	/// Provides functionality to read (heterogenously typed) cryptographic objects from PEM-formatted data.
+	/// </summary>
 	public class PemObjectReader {
 		private readonly PemReader wrapped;
 
+		/// <summary>
+		/// Constructs a PEM reader to read from <paramref name="pemDataReader"/>.
+		/// Only supports reading unencrypted objects.
+		/// </summary>
+		/// <param name="pemDataReader">A <see cref="TextReader"/> containing PEM-encoded data.</param>
 		public PemObjectReader(TextReader pemDataReader) {
 			wrapped = new PemReader(pemDataReader);
 		}
+		/// <summary>
+		/// Constructs a PEM reader to read from <paramref name="pemDataReader"/>.
+		/// Supports reading encrypted objects using <paramref name="passwordGetter"/> to obtain the password if needed.
+		/// </summary>
+		/// <param name="pemDataReader">A <see cref="TextReader"/> containing PEM-encoded data.</param>
+		/// <param name="passwordGetter">Invoked when a password is needed to decrypt an object.</param>
 		public PemObjectReader(TextReader pemDataReader, Func<char[]> passwordGetter) {
 			wrapped = new PemReader(pemDataReader, new PemHelper.FuncPasswordFinder(passwordGetter));
 		}
 
+		/// <summary>
+		/// Reads the next object from the PEM data.
+		/// </summary>
+		/// <returns>
+		/// The read object, can be a <see cref="PublicKey"/>, <see cref="PrivateKey"/>, <see cref="KeyPair"/>, or <see cref="Certificate"/>.
+		/// If there was no further object to read, <see langword="null"/> is returned.
+		/// </returns>
+		/// <exception cref="PemException">If the next object in the PEM data is of a not supported type or when an error is encountered while reading the PEM data itself.</exception>
 		public object? ReadNextObject() {
 			try {
 				var obj = wrapped.ReadObject();
@@ -43,6 +66,10 @@ namespace SGL.Utilities.Crypto {
 			}
 		}
 
+		/// <summary>
+		/// Reads all objects from the PEM data, as if by continously calling <see cref="ReadNextObject()"/> until null is returned from it.
+		/// </summary>
+		/// <returns>An enumerable of the read objects.</returns>
 		public IEnumerable<object> ReadAllObjects() {
 			object? obj;
 			while ((obj = ReadNextObject()) != null) {
--- SGL.Utilities.Crypto/PemObjectWriter.cs
+++ SGL.Utilities.Crypto/PemObjectWriter.cs
@@ -12,6 +12,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SGL.Utilities.Crypto {
+	/// <summary>
+	/// Provides functionality to write (heterogenously typed) cryptographic objects as PEM-formatted data.
+	/// </summary>
 	public class PemObjectWriter {
 		private TextWriter rawPemWriter;
 		private readonly PemWriter wrapped;
@@ -19,10 +22,27 @@ namespace SGL.Utilities.Crypto {
 		private readonly Func<char[]>? privateKeyPasswordGetter = null;
 		private readonly RandomGenerator? random = null;
 
+		/// <summary>
+		/// Contructs a PEM writer to write to <paramref name="pemDataWriter"/>.
+		/// Writing encrypted objects is not supported with this overload, as no password is taken.
+		/// Thus, sensitive objects like private keys and key pairs (containing private keys) can't be written with the resulting writer.
+		/// To write such objects, use <see cref="PemObjectWriter(TextWriter, PemEncryptionMode, Func{char[]}, RandomGenerator)"/>
+		/// </summary>
+		/// <param name="pemDataWriter">The <see cref="TextWriter"/> to which the PEM data shall be written.</param>
 		public PemObjectWriter(TextWriter pemDataWriter) {
 			rawPemWriter = pemDataWriter;
 			wrapped = new PemWriter(pemDataWriter);
 		}
+
+		/// <summary>
+		/// Contructs a PEM writer to write to <paramref name="pemDataWriter"/>.
+		/// Writing encrypted objects is supported with this overload using the given <paramref name="privateKeyEncryptionMode"/> and a password obtained from <paramref name="privateKeyPasswordGetter"/>.
+		/// </summary>
+		/// <param name="pemDataWriter">The <see cref="TextWriter"/> to which the PEM data shall be written.</param>
+		/// <param name="privateKeyEncryptionMode">The mode with which private keys and key pairs shall be encrypted.</param>
+		/// <param name="privateKeyPasswordGetter">A function object that is invoked when a private key (or key pair) is encrypted, to obtain the encryption password.</param>
+		/// <param name="random">The random genartor to use for generating an initialization vector when encrypting a private key (or key pair).</param>
+		/// <exception cref="PemException">When an unsupported <see cref="PemEncryptionMode"/> is specified.</exception>
 		public PemObjectWriter(TextWriter pemDataWriter, PemEncryptionMode privateKeyEncryptionMode, Func<char[]> privateKeyPasswordGetter, RandomGenerator random) {
 			rawPemWriter = pemDataWriter;
 			wrapped = new PemWriter(pemDataWriter);
@@ -34,6 +54,15 @@ namespace SGL.Utilities.Crypto {
 			this.random = random;
 		}
 
+		/// <summary>
+		/// Writes the given object as PEM data.
+		/// </summary>
+		/// <param name="obj">
+		/// The object to write. Supported cryptographic object types are: <see cref="PublicKey"/>, <see cref="PrivateKey"/>, <see cref="KeyPair"/>, and <see cref="Certificate"/>.
+		/// Additionally, writing a <see cref="string"/> is supported to write simple text to the PEM file as a kind of comment.
+		/// This text outside of BEGIN-END-blocks is ignored by PEM format. Thus the text will be present in the file but will not be read back by <see cref="PemObjectReader"/>.
+		/// </param>
+		/// <exception cref="PemException">When the object could not be written to PEM data successfully.</exception>
 		public void WriteObject(object obj) {
 			try {
 				switch (obj) {
@@ -78,6 +107,10 @@ namespace SGL.Utilities.Crypto {
 			}
 		}
 
+		/// <summary>
+		/// Writes the given objects to the PEM data output, as if by writing each one using <see cref="WriteObject(object)"/>.
+		/// </summary>
+		/// <param name="objects">The objects to write.</param>
 		public void WriteAllObjects(IEnumerable<object> objects) {
 			foreach (object obj in objects) {
 				WriteObject(obj);
