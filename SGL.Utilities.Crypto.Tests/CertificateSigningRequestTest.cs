6aff109161b3cc79f2c762cd72723f05d3730230
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-13 20:20:04 +0200

    Output CSRs and certificates during CSR test.

--- SGL.Utilities.Crypto.Tests/CertificateSigningRequestTest.cs
+++ SGL.Utilities.Crypto.Tests/CertificateSigningRequestTest.cs
@@ -2,10 +2,12 @@
 using SGL.Utilities.Crypto.Keys;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace SGL.Utilities.Crypto.Tests {
 	public class CertificateSigningRequestTest {
@@ -15,8 +17,9 @@ namespace SGL.Utilities.Crypto.Tests {
 		public Certificate SignerCertificate { get; }
 		public KeyPair SubjectKeyPair { get; }
 		public DistinguishedName SubjectDN { get; }
+		public ITestOutputHelper Output { get; }
 
-		public CertificateSigningRequestTest() {
+		public CertificateSigningRequestTest(ITestOutputHelper output) {
 			Random = new RandomGenerator();
 			SignerKeyPair = KeyPair.GenerateEllipticCurves(Random, 521);
 			SignerDN = new DistinguishedName(new KeyValuePair<string, string>[] { new("o", "SGL"), new("ou", "Utility"), new("ou", "Tests"), new("cn", "Test Signer") });
@@ -24,13 +27,25 @@ namespace SGL.Utilities.Crypto.Tests {
 				authorityKeyIdentifier: new KeyIdentifier(SignerKeyPair.Public), generateSubjectKeyIdentifier: true, keyUsages: KeyUsages.KeyCertSign, caConstraint: (true, 0));
 			SubjectKeyPair = KeyPair.GenerateEllipticCurves(Random, 521);
 			SubjectDN = new DistinguishedName(new KeyValuePair<string, string>[] { new("o", "SGL"), new("ou", "Utility"), new("ou", "Tests"), new("cn", "Test 1") });
+			Output = output;
+		}
+
+		private void DumpToOutput(object obj) {
+			using var strWriter = new StringWriter();
+			var writer = new PemObjectWriter(strWriter);
+			writer.WriteObject(obj);
+			Output.WriteLine(strWriter.ToString());
 		}
 
 		[Fact]
 		public void BasicCsrCanBeGeneratedAndSignedIntoCertificateSuccessfully() {
 			var csr = CertificateSigningRequest.Generate(SubjectDN, SubjectKeyPair);
+			Output.WriteLine("CSR:");
+			DumpToOutput(csr);
 			var policy = new CsrSigningPolicy(Random);
 			var cert = csr.GenerateCertificate(SignerCertificate, SignerKeyPair, policy);
+			Output.WriteLine("Certificate:");
+			DumpToOutput(cert);
 			Assert.Equal(SignerDN, cert.IssuerDN);
 			Assert.Equal(SubjectDN, cert.SubjectDN);
 			Assert.Equal(SubjectKeyPair.Public, cert.PublicKey);
@@ -39,8 +54,12 @@ namespace SGL.Utilities.Crypto.Tests {
 		[Fact]
 		public void CsrWithExtensionsCanBeGeneratedAndSignedIntoCertificateSuccessfully() {
 			var csr = CertificateSigningRequest.Generate(SubjectDN, SubjectKeyPair, requestSubjectKeyIdentifier: true, requestAuthorityKeyIdentifier: true, requestKeyUsages: KeyUsages.KeyEncipherment, requestCABasicConstraints: (false, null));
+			Output.WriteLine("CSR:");
+			DumpToOutput(csr);
 			var policy = new CsrSigningPolicy(Random);
 			var cert = csr.GenerateCertificate(SignerCertificate, SignerKeyPair, policy);
+			Output.WriteLine("Certificate:");
+			DumpToOutput(cert);
 			Assert.Equal(SignerDN, cert.IssuerDN);
 			Assert.Equal(SubjectDN, cert.SubjectDN);
 			Assert.Equal(SubjectKeyPair.Public, cert.PublicKey);
