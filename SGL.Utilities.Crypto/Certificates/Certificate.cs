29b8a4df48d58e3551b2208d4065a26d9e9207b8
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-13 17:17:22 +0200

    Extracted key usages mapping for reuse in CSR.

--- SGL.Utilities.Crypto/Certificates/Certificate.cs
+++ SGL.Utilities.Crypto/Certificates/Certificate.cs
@@ -109,45 +109,16 @@ namespace SGL.Utilities.Crypto.Certificates {
 					if (keyUsageEnc != null) {
 						var keyUsageExtension = KeyUsage.GetInstance(keyUsageEnc.GetOctets());
 						var keyUsageBitFlags = keyUsageExtension.IntValue;
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.DigitalSignature, ref usages);
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.NonRepudiation, ref usages);
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.KeyEncipherment, ref usages);
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.DataEncipherment, ref usages);
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.KeyAgreement, ref usages);
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.KeyCertSign, ref usages);
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.CrlSign, ref usages);
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.EncipherOnly, ref usages);
-						SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.DecipherOnly, ref usages);
+						CertificateHelpers.MapBasicKeyUsageFlags(ref usages, keyUsageBitFlags);
 						keyUsageCache = usages;
 					}
 					if (extKeyUsageEnc != null) {
 						var extKeyUsageExtension = ExtendedKeyUsage.GetInstance(extKeyUsageEnc.GetOctets());
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPServerAuth, KeyUsages.ExtServerAuth, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPClientAuth, KeyUsages.ExtClientAuth, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPCodeSigning, KeyUsages.ExtCodeSigning, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPEmailProtection, KeyUsages.ExtEmailProtection, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPIpsecEndSystem, KeyUsages.ExtIpsecEndSystem, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPIpsecTunnel, KeyUsages.ExtIpsecTunnel, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPIpsecUser, KeyUsages.ExtIpsecUser, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPTimeStamping, KeyUsages.ExtTimeStamping, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPOcspSigning, KeyUsages.ExtOcspSigning, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPSmartCardLogon, KeyUsages.ExtSmartCardLogon, ref usages);
-						SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.AnyExtendedKeyUsage, KeyUsages.ExtAnyPurpose, ref usages);
+						CertificateHelpers.MapExtendedKeyUsageFlags(ref usages, extKeyUsageExtension);
 						keyUsageCache = usages;
 					}
 				}
 				return keyUsageCache;
-			}
-		}
-
-		private static void SetBitIfUsagePresent(ExtendedKeyUsage extKeyUsages, KeyPurposeID keyPurposeId, KeyUsages usage, ref KeyUsages flagsToSet) {
-			if (extKeyUsages.HasKeyPurposeId(keyPurposeId)) {
-				flagsToSet |= usage;
-			}
-		}
-		private static void SetBitIfUsagePresent(int keyUsageBitFlags, KeyUsages usage, ref KeyUsages flagsToSet) {
-			if ((keyUsageBitFlags & (int)(usage & ~KeyUsages.AllSupportedExt)) != 0) {
-				flagsToSet |= usage;
 			}
 		}
 
---
+++ SGL.Utilities.Crypto/Certificates/CertificateHelpers.cs
@@ -0,0 +1,43 @@
+﻿using Org.BouncyCastle.Asn1.X509;
+
+namespace SGL.Utilities.Crypto.Certificates {
+	internal static class CertificateHelpers {
+
+		internal static void MapBasicKeyUsageFlags(ref KeyUsages usages, int keyUsageBitFlags) {
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.DigitalSignature, ref usages);
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.NonRepudiation, ref usages);
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.KeyEncipherment, ref usages);
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.DataEncipherment, ref usages);
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.KeyAgreement, ref usages);
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.KeyCertSign, ref usages);
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.CrlSign, ref usages);
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.EncipherOnly, ref usages);
+			SetBitIfUsagePresent(keyUsageBitFlags, KeyUsages.DecipherOnly, ref usages);
+		}
+
+		internal static void MapExtendedKeyUsageFlags(ref KeyUsages usages, ExtendedKeyUsage extKeyUsageExtension) {
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPServerAuth, KeyUsages.ExtServerAuth, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPClientAuth, KeyUsages.ExtClientAuth, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPCodeSigning, KeyUsages.ExtCodeSigning, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPEmailProtection, KeyUsages.ExtEmailProtection, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPIpsecEndSystem, KeyUsages.ExtIpsecEndSystem, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPIpsecTunnel, KeyUsages.ExtIpsecTunnel, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPIpsecUser, KeyUsages.ExtIpsecUser, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPTimeStamping, KeyUsages.ExtTimeStamping, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPOcspSigning, KeyUsages.ExtOcspSigning, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.IdKPSmartCardLogon, KeyUsages.ExtSmartCardLogon, ref usages);
+			SetBitIfUsagePresent(extKeyUsageExtension, KeyPurposeID.AnyExtendedKeyUsage, KeyUsages.ExtAnyPurpose, ref usages);
+		}
+
+		internal static void SetBitIfUsagePresent(ExtendedKeyUsage extKeyUsages, KeyPurposeID keyPurposeId, KeyUsages usage, ref KeyUsages flagsToSet) {
+			if (extKeyUsages.HasKeyPurposeId(keyPurposeId)) {
+				flagsToSet |= usage;
+			}
+		}
+		internal static void SetBitIfUsagePresent(int keyUsageBitFlags, KeyUsages usage, ref KeyUsages flagsToSet) {
+			if ((keyUsageBitFlags & (int)(usage & ~KeyUsages.AllSupportedExt)) != 0) {
+				flagsToSet |= usage;
+			}
+		}
+	}
+}
