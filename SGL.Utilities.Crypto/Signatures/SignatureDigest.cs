aa4d0912d306e04df10777028a9602dfc3f812f5
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-09-14 19:44:46 +0200

    Defined SignatureDigest based on CertificateSignatureDigest.

---
+++ SGL.Utilities.Crypto/Signatures/SignatureDigest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGL.Utilities.Crypto.Signatures {
+	public enum SignatureDigest {
+		/// <summary>
+		/// Represents SHA-256.
+		/// </summary>
+		Sha256 = 2,
+		/// <summary>
+		/// Represents SHA-384.
+		/// </summary>
+		Sha384 = 3,
+		/// <summary>
+		/// Represents SHA-512.
+		/// </summary>
+		Sha512 = 5
+	}
+}
