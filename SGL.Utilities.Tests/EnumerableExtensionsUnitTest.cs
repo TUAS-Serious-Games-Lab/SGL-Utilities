c39b99c76ff9e80bd8f8f4a959d382f72f174e70
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-01-20 15:16:19 +0100

    Added unit tests for MapBufferedAsync.

---
+++ SGL.Utilities.Tests/EnumerableExtensionsUnitTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SGL.Utilities.Tests {
+	public class EnumerableExtensionsUnitTest {
+		private ITestOutputHelper testLog;
+
+		public EnumerableExtensionsUnitTest(ITestOutputHelper testLog) {
+			this.testLog = testLog;
+		}
+
+		[Fact]
+		public async Task MapBufferedAsyncCorrectlyDrivesTasksWithTheSpecifiedConcurrency() {
+			int concurrencyCounter = 0;
+			var output = Enumerable.Range(0, 8).MapBufferedAsync(4, async idx => {
+				var concurrency = Interlocked.Increment(ref concurrencyCounter);
+				await Task.Delay(500);
+				Interlocked.Decrement(ref concurrencyCounter);
+				return (idx, concurrency);
+			});
+			int idxExpected = 0;
+			var concurrencyValues = new List<int>();
+			await foreach (var (idxActual, concurrency) in output) {
+				testLog.WriteLine($"{idxActual}: {concurrency} {concurrencyCounter}");
+				Assert.InRange(concurrency, 1, 4);
+				Assert.Equal(idxExpected++, idxActual);
+				concurrencyValues.Add(concurrency);
+			}
+			Assert.Contains(concurrencyValues, c => c == 4);
+		}
+		[Fact]
+		public async Task MapBufferedAsyncKeepsOrderingUnderVaryingTaskRuntimes() {
+			var output = Enumerable.Range(0, 8).MapBufferedAsync(4, async idx => {
+				await Task.Delay(800 - 100 * idx);
+				testLog.WriteLine($"{idx} completed");
+				return idx;
+			});
+			int idxExpected = 0;
+			await foreach (var idxActual in output) {
+				Assert.Equal(idxExpected++, idxActual);
+			}
+		}
+		[Fact]
+		public async Task MapBufferedAsyncTerminatesCleanlyOnAsyncExceptions() {
+			int pendingCounter = 0;
+			var taskStarted = new bool[16];
+			var output = Enumerable.Range(0, 16).MapBufferedAsync(4, async idx => {
+				taskStarted[idx] = true;
+				if (idx == 6) {
+					await Task.Delay(300);
+					throw new Exception("Oops");
+				}
+				else {
+					Interlocked.Increment(ref pendingCounter);
+					await Task.Delay(800);
+					Interlocked.Decrement(ref pendingCounter);
+					return idx;
+				}
+			});
+			var outputList = new List<int>();
+			Exception? caughtException = null;
+			try {
+				await foreach (var idx in output) {
+					testLog.WriteLine($"{idx} read");
+					outputList.Add(idx);
+				}
+			}
+			catch (Exception ex) {
+				testLog.WriteLine(ex.ToString());
+				caughtException = ex;
+			}
+			Assert.Equal(0, pendingCounter);
+			Assert.Equal(Enumerable.Range(0, 6), outputList);
+			Assert.NotNull(caughtException);
+			Assert.Equal("Oops", caughtException.Message);
+			Assert.False(taskStarted.Last());
+			testLog.WriteLine("Started tasks: " + string.Join("", taskStarted.Select(s => s ? "Y" : "N")));
+		}
+		[Fact]
+		public async Task MapBufferedAsyncTerminatesCleanlyOnSyncExceptions() {
+			int pendingCounter = 0;
+			var taskStarted = new bool[16];
+			var output = Enumerable.Range(0, 16).MapBufferedAsync(4, idx => {
+				taskStarted[idx] = true;
+				if (idx == 6) {
+					throw new Exception("Oops");
+				}
+				else {
+					Interlocked.Increment(ref pendingCounter);
+					return Task.Run(async () => {
+						await Task.Delay(800);
+						Interlocked.Decrement(ref pendingCounter);
+						return idx;
+					});
+				}
+			});
+			var outputList = new List<int>();
+			Exception? caughtException = null;
+			try {
+				await foreach (var idx in output) {
+					testLog.WriteLine($"{idx} read");
+					outputList.Add(idx);
+				}
+			}
+			catch (Exception ex) {
+				testLog.WriteLine(ex.ToString());
+				caughtException = ex;
+			}
+			Assert.Equal(0, pendingCounter);
+			Assert.Equal(Enumerable.Range(0, 6), outputList);
+			Assert.NotNull(caughtException);
+			Assert.Equal("Oops", caughtException.Message);
+			Assert.False(taskStarted.Last());
+			testLog.WriteLine("Started tasks: " + string.Join("", taskStarted.Select(s => s ? "Y" : "N")));
+		}
+	}
+}
