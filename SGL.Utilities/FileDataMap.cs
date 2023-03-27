51beb2d85b59ee8b9551562624de184ececb1c03
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-03-27 13:39:09 +0200

    Added API docs for FileDataMap.

--- SGL.Utilities/FileDataMap.cs
+++ SGL.Utilities/FileDataMap.cs
@@ -9,9 +9,30 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace SGL.Utilities {
+	/// <summary>
+	/// Provides file handling logic for storing key-associated values of type <typeparamref name="TValue"/> persistently in separate files under a common directory.
+	/// The keys are of type <typeparamref name="TKey"/>.
+	/// This class handles the storage aspects of the functionality.
+	/// The actual (de-)serialization of the values, as well as the mapping from <typeparamref name="TKey"/> to the relative path under the directory,
+	/// are handled by delegates taken at construction.
+	///
+	/// Write operations are protected against failures resulting in incomplete files by writing to a temporary file first and then atomically replacing the old file.
+	///
+	/// None of the I/O operations run on the calling thread of the operation methods of this class,
+	/// instead all I/O is dispatched to the threadpool, even those where only synchronous APIs are available, like opening a file stream.
+	///
+	/// There is also (still experimental) optional support for concurrent file access using a lock files for write-write synchronization and
+	/// retry loops with file sharing restrictions for read-write and write-read synchronization.
+	/// </summary>
+	/// <typeparam name="TKey">The type of the keys that identify the values.</typeparam>
+	/// <typeparam name="TValue">The type of the value to store, must be a non-nullable reference type.</typeparam>
 	public class FileDataMap<TKey, TValue> where TKey : notnull where TValue : class {
 		private const string tempSeparator = ".temp-";
 		private const string lockFileSuffix = ".lockfile";
+
+		/// <summary>
+		/// The path of the directory under which the values are stored.
+		/// </summary>
 		public string DirectoryPath { get; }
 		/// <summary>
 		/// When using concurrent mode, sets the interval for retry loops.
@@ -22,7 +43,13 @@ namespace SGL.Utilities {
 		/// </summary>
 		public TimeSpan WaitTimeout { get; set; } = TimeSpan.FromSeconds(1);
 
+		/// <summary>
+		/// The logger used by internal operations of this class, defaults to <see cref="NullLogger.Instance"/>.
+		/// </summary>
 		public ILogger Logger { get; set; } = NullLogger.Instance;
+		/// <summary>
+		/// How to refer to what is stored in the file for logging purposes, must contain a <c>{placeholder}</c> for the key.
+		/// </summary>
 		public string FileTerminology { get; set; } = "file {key}";
 
 		private Func<Stream, Task<TValue>> readContent;
@@ -53,9 +80,34 @@ namespace SGL.Utilities {
 			/// </summary>
 			public TValue WrittenValue { get; }
 		}
+		/// <summary>
+		/// Provides an event hook that is triggered after a temporary file was written, before it is moved to the final file.
+		/// Note: The event handlers are invoked from a threadpool thread. If this requires synchronization, it must be done in the handlers.
+		/// </summary>
 		public event AsyncEventHandler<TemporaryFileWrittenEventArgs>? TemporaryFileWritten;
 		private StringGenerator tempSuffixGenerator = new StringGenerator();
 
+		/// <summary>
+		/// Constructs a <see cref="FileDataMap{TKey, TValue}"/> with the given parameters.
+		/// </summary>
+		/// <param name="directoryPath">
+		/// The path of the directory under which the values are stored.
+		/// The path of the file for each key relative to this is determined by <paramref name="getFilePath"/>.
+		/// Temporary files and lock files will have an appended suffix added to the resulting paths.
+		/// </param>
+		/// <param name="readContent">
+		/// A delegate that implements the logic for reading a <typeparamref name="TValue"/> from a <see cref="Stream"/> asynchronously.
+		/// Note: The delegate is invoked from a threadpool thread. If this requires synchronization, it must be done in the delegate.
+		/// </param>
+		/// <param name="writeContent">
+		/// A delegate that implements the logic for writing a <typeparamref name="TValue"/> to a <see cref="Stream"/> asynchronously.
+		/// Note: The delegate is invoked from a threadpool thread. If this requires synchronization, it must be done in the delegate.
+		/// </param>
+		/// <param name="getFilePath">
+		/// A delegate that implements the mapping of keys to relative file paths under <paramref name="directoryPath"/>.
+		/// Note: Unlike <paramref name="readContent"/> and <paramref name="writeContent"/>, this delegate is invoked on the calling thread of each operation method.
+		/// </param>
+		/// <param name="concurrent">Whether to enable the concurrent mode.</param>
 		public FileDataMap(string directoryPath, Func<Stream, Task<TValue>> readContent, Func<Stream, TValue, Task> writeContent, Func<TKey, string>? getFilePath = null, bool concurrent = false) {
 			DirectoryPath = directoryPath;
 			this.readContent = readContent;
@@ -67,6 +119,12 @@ namespace SGL.Utilities {
 			this.concurrent = concurrent;
 		}
 
+		/// <summary>
+		/// Asynchronously determines if the underlying file for the given <paramref name="key"/> is present.
+		/// </summary>
+		/// <param name="key">The key to check.</param>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation, containing bool indicating whether the file is present.</returns>
 		public Task<bool> IsPresentAsync(TKey key, CancellationToken ct = default) {
 			var filePath = getFilePath(key);
 			return Task.Run(() => {
@@ -79,6 +137,13 @@ namespace SGL.Utilities {
 				}
 			}, ct);
 		}
+
+		/// <summary>
+		/// Asynchronously deletes the underlying file for the given <paramref name="key"/>, removing the stored value.
+		/// </summary>
+		/// <param name="key">The key for which to remove the value.</param>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation.</returns>
 		public Task RemoveAsync(TKey key, CancellationToken ct = default) {
 			var filePath = getFilePath(key);
 			return Task.Run(async () => {
@@ -118,6 +183,13 @@ namespace SGL.Utilities {
 				}
 			}, ct);
 		}
+		/// <summary>
+		/// Asynchronously stores <paramref name="value"/> in the file for key <paramref name="key"/>.
+		/// </summary>
+		/// <param name="key">The key under which to store <paramref name="value"/>.</param>
+		/// <param name="value">The value to store.</param>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation.</returns>
 		public Task StoreValueAsync(TKey key, TValue value, CancellationToken ct = default) {
 			var filePath = getFilePath(key);
 			return Task.Run(async () => {
@@ -158,10 +230,30 @@ namespace SGL.Utilities {
 				}
 			}, ct);
 		}
+		/// <summary>
+		/// Asynchronously opens the raw underlying file for <paramref name="key"/> for reading.
+		/// This method is suitable only in rare cases, namely when <typeparamref name="TValue"/> is a <see cref="Stream"/> or similar.
+		/// In this case, the write deledgate can just copy to the temp file stream and reading the content can be done using this method,
+		/// bypassing the read delegate and allowing streamed read access.
+		/// Using <see cref="GetValueAsync(TKey, CancellationToken)"/> for this would require copying the content into a <see cref="MemoryStream"/>
+		/// because there, the file is closed at the end of the call.
+		/// With this method, it is the responsibility of the caller to close / dispose the returned stream object.
+		/// In such cases, the benefit of using <see cref="FileDataMap{TKey, TValue}"/> over direct file access is the automation of temp file handling on the writing side.
+		/// </summary>
+		/// <param name="key">The key for which to open the associated file.</param>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation, containing a readable <see cref="Stream"/> for the underlying file,
+		/// or null if no file is present for <paramref name="key"/>.</returns>
 		public Task<Stream?> OpenRawReadAsync(TKey key, CancellationToken ct = default) {
 			var filePath = getFilePath(key);
 			return Task.Run(() => OpenRawReadInnerAsync(key, filePath, ct), ct);
 		}
+		/// <summary>
+		/// Asynchronously reads the value associated with <paramref name="key"/> from the underlying file.
+		/// </summary>
+		/// <param name="key">The key for which to read the value.</param>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation, containing the read value, or null if no file is present for <paramref name="key"/>.</returns>
 		public Task<TValue?> GetValueAsync(TKey key, CancellationToken ct = default) {
 			var filePath = getFilePath(key);
 			return Task.Run(async Task<TValue?> () => {
@@ -173,6 +265,17 @@ namespace SGL.Utilities {
 				}
 			}, ct);
 		}
+		/// <summary>
+		/// Asynchronously updates the value associated with <paramref name="key"/> by reading the value form its file, applying <paramref name="update"/> to it and writing the modified value back to the file.
+		/// </summary>
+		/// <param name="key">The key for which to update the value.</param>
+		/// <param name="update">
+		/// A delegate containing the update logic to apply to the value.
+		/// When running in concurrent mode, this delegate may be called multiple times due to retries.
+		/// </param>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation.</returns>
+		/// <exception cref="InvalidOperationException">If there was no previous value for <paramref name="key"/> that could be updated.</exception>
 		public Task UpdateValueAsync(TKey key, Action<TValue> update, CancellationToken ct = default) {
 			var filePath = getFilePath(key);
 			return Task.Run(async () => {
