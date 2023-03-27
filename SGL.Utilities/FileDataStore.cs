b34b92e40d40c2ea0859dc0f3a9e3bfa14877419
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-03-27 10:30:11 +0200

    Added API docs to FileDataStore.

--- SGL.Utilities/FileDataStore.cs
+++ SGL.Utilities/FileDataStore.cs
@@ -9,32 +9,82 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace SGL.Utilities {
+	/// <summary>
+	/// Provides file handling logic for storing a value of type <typeparamref name="TValue"/> persistently in a file.
+	/// This class handles the storage aspects of the functionality.
+	/// The actual (de-)serialization of the value is handled by delegates taken at construction.
+	///
+	/// Write operations are protected against failures resulting in incomplete files by writing to a temporary file first and then atomically replacing the old file.
+	///
+	/// There is also (still experimantal) optional support for concurrent file access using a lock file for write-write synchronization and
+	/// retry loops with file sharing restrictions for read-write and write-read synchronization.
+	/// </summary>
+	/// <typeparam name="TValue">The type of the value to store, must be a non-nullable reference type.</typeparam>
 	public class FileDataStore<TValue> where TValue : class {
 		private const string tempSeparator = ".temp-";
 		private const string lockFileSuffix = ".lockfile";
+		/// <summary>
+		/// When using concurrent mode, sets the interval for retry loops.
+		/// </summary>
 		public TimeSpan PollingInterval { get; set; } = TimeSpan.FromMilliseconds(1);
+		/// <summary>
+		/// When using concurrent mode, sets the timeout for acquiring a lock file or for succeeding in a retry loop.
+		/// </summary>
 		public TimeSpan WaitTimeout { get; set; } = TimeSpan.FromSeconds(1);
+		/// <summary>
+		/// The path of the underlying file in which the value is stored.
+		/// </summary>
 		public string FilePath { get; }
 
+		/// <summary>
+		/// The logger used by internal operations of this class, defaults to <see cref="NullLogger.Instance"/>.
+		/// </summary>
 		public ILogger Logger { get; set; } = NullLogger.Instance;
+
+		/// <summary>
+		/// How to refer to what is stored in the file for logging purposes.
+		/// </summary>
 		public string FileTerminology { get; set; } = "file";
 
 		private Func<Stream, Task<TValue>> readContent;
 		private Func<Stream, TValue, Task> writeContent;
 		private readonly bool concurrent;
 
+		/// <summary>
+		/// The event arguments passed to <see cref="TemporaryFileWritten"/>.
+		/// </summary>
 		public class TemporaryFileWrittenEventArgs : EventArgs {
 			internal TemporaryFileWrittenEventArgs(string temporaryFilePath, TValue writtenValue) {
 				TemporaryFilePath = temporaryFilePath;
 				WrittenValue = writtenValue;
 			}
 
+			/// <summary>
+			/// The path of the temporary file that was just written.
+			/// </summary>
 			public string TemporaryFilePath { get; }
+			/// <summary>
+			/// The value object written to the file.
+			/// </summary>
 			public TValue WrittenValue { get; }
 		}
+		/// <summary>
+		/// Provides an event hook that is triggered after a temporary file was written, before it is moved to the final file.
+		/// </summary>
 		public event AsyncEventHandler<TemporaryFileWrittenEventArgs>? TemporaryFileWritten;
+
 		private StringGenerator tempSuffixGenerator = new StringGenerator();
 
+		/// <summary>
+		/// Constructs a <see cref="FileDataStore{TValue}"/> with the given parameters.
+		/// </summary>
+		/// <param name="filePath">
+		/// The path of the underlying file to use.
+		/// Temporary files and the lock file will have an appended suffix added to this path and filename.
+		/// </param>
+		/// <param name="readContent">A delegate that implements the logic for reading a <typeparamref name="TValue"/> from a <see cref="Stream"/> asynchronously.</param>
+		/// <param name="writeContent">A delegate that implements the logic for writing a <typeparamref name="TValue"/> to a <see cref="Stream"/> asynchronously.</param>
+		/// <param name="concurrent">Whether to enable the concurrent mode.</param>
 		public FileDataStore(string filePath, Func<Stream, Task<TValue>> readContent, Func<Stream, TValue, Task> writeContent, bool concurrent = false) {
 			FilePath = filePath;
 			this.readContent = readContent;
@@ -42,6 +92,11 @@ namespace SGL.Utilities {
 			this.concurrent = concurrent;
 		}
 
+		/// <summary>
+		/// Asynchronously determines if the underlying file is present.
+		/// </summary>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation, containing bool indicating whether the file is present.</returns>
 		public Task<bool> IsPresentAsync(CancellationToken ct = default) {
 			return Task.Run(() => {
 				try {
@@ -53,6 +108,11 @@ namespace SGL.Utilities {
 				}
 			}, ct);
 		}
+		/// <summary>
+		/// Asynchronously deletes the underlying file, removing the stored value.
+		/// </summary>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation.</returns>
 		public Task ClearAsync(CancellationToken ct = default) {
 			return Task.Run(async () => {
 				try {
@@ -91,6 +151,12 @@ namespace SGL.Utilities {
 				}
 			}, ct);
 		}
+		/// <summary>
+		/// Asynchronously stores <paramref name="value"/> in the file.
+		/// </summary>
+		/// <param name="value">The value to store.</param>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation.</returns>
 		public Task StoreValueAsync(TValue value, CancellationToken ct = default) {
 			return Task.Run(async () => {
 				var tempFile = GetTempFilePath();
@@ -220,9 +286,27 @@ namespace SGL.Utilities {
 			}
 		}
 
+		/// <summary>
+		/// Asynchronously opens the raw underlying file for reading.
+		/// This method is suitable only in rare cases, namely when <typeparamref name="TValue"/> is a <see cref="Stream"/> or similar.
+		/// In this case, the write deledgate can just copy to the temp file stream and reading the content can be done using this method,
+		/// bypassing the read delegate and allowing streamed read access.
+		/// Using <see cref="GetValueAsync(CancellationToken)"/> for this would require copying the content into a <see cref="MemoryStream"/>
+		/// because the there, the file is closed at the end of the call.
+		/// With this method, it is the responsibility of the caller to close / dispose the returned stream object.
+		/// In such cases, the benefit of using <see cref="FileDataStore{TValue}"/> over direct file access is the automation of temp file handling on the writing side.
+		/// </summary>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation, containing a readable <see cref="Stream"/> for the underlying file, or null if no file is present.</returns>
 		public Task<Stream?> OpenRawReadAsync(CancellationToken ct = default) {
 			return Task.Run(() => OpenRawReadInnerAsync(ct), ct);
 		}
+
+		/// <summary>
+		/// Asynchronously reads the value from the underlying file.
+		/// </summary>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation, containing the read value, or null if no file is present.</returns>
 		public Task<TValue?> GetValueAsync(CancellationToken ct = default) {
 			return Task.Run(async Task<TValue?> () => {
 				await using (var fileStream = await OpenRawReadInnerAsync(ct)) {
@@ -234,6 +318,16 @@ namespace SGL.Utilities {
 			}, ct);
 		}
 
+		/// <summary>
+		/// Asynchronously updates the value in the file by reading the value, applying <paramref name="update"/> to it and writing the modified value back to the file.
+		/// </summary>
+		/// <param name="update">
+		/// A delegate containing the update logic to apply to the value.
+		/// When running in concurrent mode, this delegate may be called multiple times due to retries.
+		/// </param>
+		/// <param name="ct">A <see cref="CancellationToken"/> to allow canceling the operation.</param>
+		/// <returns>A <see cref="Task{TResult}"/> for the operation.</returns>
+		/// <exception cref="InvalidOperationException">If there was no previous value that could be updated.</exception>
 		public Task UpdateValueAsync(Action<TValue> update, CancellationToken ct = default) {
 			return Task.Run(async () => {
 				if (!File.Exists(FilePath)) {
