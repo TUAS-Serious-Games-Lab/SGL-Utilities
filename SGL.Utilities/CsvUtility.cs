e3e2e018fe11ab83c7d106114fdd686600cb1803
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2023-10-23 16:43:31 +0200

    Added read and write methods for only the header or for single entries, and read methods that asynchronously enumerate entries to CsvUtility.

--- SGL.Utilities/CsvUtility.cs
+++ SGL.Utilities/CsvUtility.cs
@@ -35,6 +35,31 @@ namespace SGL.Utilities {
 			}
 		}
 
+		/// <summary>
+		/// Asynchronously writes the given <paramref name="columns"/> to <paramref name="output"/> as a CSV header line.
+		/// </summary>
+		public static async Task WriteCsvHeaderAsync(TextWriter output, List<string> columns, CancellationToken ct = default) {
+			var line = string.Join(";", columns.Select(EscapeString));
+			await output.WriteLineAsync(line.AsMemory(), ct).ConfigureAwait(false);
+		}
+
+		/// <summary>
+		/// Asynchronously writes the given <paramref name="entry"/> to <paramref name="output"/> as a CSV entry line.
+		/// The fields (keyed by column name) are writen in the order represented in <paramref name="columns"/>.
+		///	Fields not mentioned in <paramref name="columns"/> are not written.
+		/// </summary>
+		public static Task WriteCsvEntryAsync(TextWriter output, List<string> columns, Dictionary<string, string> entry, CancellationToken ct = default) {
+			return WriteCsvEntryAsync(output, columns.Select(col => entry.TryGetValue(col, out var val) ? val : ""), ct);
+		}
+
+		/// <summary>
+		/// Asynchronously writes the given <paramref name="entryFields"/> to <paramref name="output"/> as a CSV entry in the given field order.
+		/// </summary>
+		public static async Task WriteCsvEntryAsync(TextWriter output, IEnumerable<string> entryFields, CancellationToken ct = default) {
+			var line = string.Join(";", entryFields.Select(EscapeString));
+			await output.WriteLineAsync(line.AsMemory(), ct).ConfigureAwait(false);
+		}
+
 		private static char[] charsToEscape = Environment.NewLine.ToCharArray().Append('"').Append(';').ToArray();
 		private static string EscapeString(string str) {
 			if (str.IndexOfAny(charsToEscape) == -1) {
@@ -76,6 +101,101 @@ namespace SGL.Utilities {
 				entries.Add(entry);
 			}
 			return (columnNames, entries);
+		}
+
+		/// <summary>
+		/// Reads a CSV header (line) from <paramref name="input"/> and returns the column names.
+		/// </summary>
+		/// <exception cref="InvalidDataException">If no line could be read.</exception>
+		public static async Task<List<string>> ReadCsvHeaderAsync(TextReader input, CancellationToken ct = default) {
+			var headerLine = await input.ReadLineAsync().ConfigureAwait(false);
+			ct.ThrowIfCancellationRequested();
+			if (headerLine == null) {
+				throw new InvalidDataException("Couldn't read header from CSV.");
+			}
+			var sb = new StringBuilder();
+			return (await ParseLine(headerLine, sb, () => input.ReadLineAsync()).ToListAsync(ct).ConfigureAwait(false)).ToList();
+		}
+
+		/// <summary>
+		/// Reads a CSV entry from <paramref name="input"/> and returns its fields in the order they were in <paramref name="input"/>.
+		/// If no entry could be read from <paramref name="input"/>, null is returned.
+		/// </summary>
+		public static async Task<List<string>?> ReadCsvEntryAsync(TextReader input, CancellationToken ct = default) {
+			var sb = new StringBuilder();
+			string? line;
+			if ((line = await input.ReadLineAsync().ConfigureAwait(false)) != null) {
+				ct.ThrowIfCancellationRequested();
+				return (await ParseLine(line, sb, () => input.ReadLineAsync()).ToListAsync(ct).ConfigureAwait(false)).ToList();
+			}
+			else {
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Reads a CSV entry from <paramref name="input"/> with the column order specified in <paramref name="columnNames"/>.
+		/// The fields of the entry are returned as a dictionary keyed by column names.
+		/// If no entry could be read from <paramref name="input"/>, null is returned.
+		/// </summary>
+		/// <exception cref="InvalidDataException">The line(s) read from <paramref name="input"/> had a number of fields different
+		/// from the number of columns indicated by <paramref name="columnNames"/>.</exception>
+		public static async Task<Dictionary<string, string>?> ReadCsvEntryAsync(TextReader input, List<string> columnNames, CancellationToken ct = default) {
+			var fields = await ReadCsvEntryAsync(input, ct);
+			if (fields == null) {
+				return null;
+			}
+			if (fields.Count != columnNames.Count) {
+				throw new InvalidDataException("Encountered line with incorrect field count.");
+			}
+			return new Dictionary<string, string>(columnNames.Zip(fields, KeyValuePair.Create));
+		}
+
+		/// <summary>
+		/// Asynchronously reads and enumerates all CSV entries from <paramref name="input"/>.
+		/// It is assumed, that the header line was already read from <paramref name="input"/>,
+		/// e.g. using <see cref="ReadCsvHeaderAsync(TextReader, CancellationToken)"/>.
+		/// The entries are returned as string lists with their fields in the order present in <paramref name="input"/>.
+		/// </summary>
+		/// <exception cref="InvalidDataException">If an entry is encountered whose field count differs from previous entries.</exception>
+		public static async IAsyncEnumerable<List<string>> ReadCsvEntriesAsync(TextReader input,
+				[EnumeratorCancellation] CancellationToken ct = default) {
+			var sb = new StringBuilder();
+			string? line;
+			int? fieldCount = null;
+			while ((line = await input.ReadLineAsync().ConfigureAwait(false)) != null) {
+				ct.ThrowIfCancellationRequested();
+				var fields = (await ParseLine(line, sb, () => input.ReadLineAsync()).ToListAsync(ct).ConfigureAwait(false)).ToList();
+				if (!fieldCount.HasValue) {
+					fieldCount = fields.Count;
+				}
+				else if (fields.Count != fieldCount.Value) {
+					throw new InvalidDataException("Encountered line with differing field count.");
+				}
+				yield return fields;
+			}
+		}
+
+		/// <summary>
+		/// Asynchronously reads and enumerates all CSV entries from <paramref name="input"/>.
+		/// It is assumed, that the header line was already read from <paramref name="input"/>,
+		/// e.g. using <see cref="ReadCsvHeaderAsync(TextReader, CancellationToken)"/>.
+		/// The entries are returned as dictionaries keyed by column name.
+		/// The column names and their ordering is indicated by <paramref name="columnNames"/>.
+		/// </summary>
+		/// <exception cref="InvalidDataException">If an entry with a field count different from the number of columns is encountered.</exception>
+		public static async IAsyncEnumerable<Dictionary<string, string>> ReadCsvEntriesAsync(TextReader input, List<string> columnNames,
+				[EnumeratorCancellation] CancellationToken ct = default) {
+			var sb = new StringBuilder();
+			string? line;
+			while ((line = await input.ReadLineAsync().ConfigureAwait(false)) != null) {
+				ct.ThrowIfCancellationRequested();
+				var fields = await ParseLine(line, sb, () => input.ReadLineAsync()).ToListAsync(ct).ConfigureAwait(false);
+				if (fields.Count != columnNames.Count) {
+					throw new InvalidDataException("Encountered line with incorrect field count.");
+				}
+				yield return new Dictionary<string, string>(columnNames.Zip(fields, KeyValuePair.Create));
+			}
 		}
 
 		private static async IAsyncEnumerable<string> ParseLine(string line, StringBuilder sb, Func<Task<string?>> readNextLine,
